630f8ee586bc89f0afffb7b18c1f39e5cbd2ab50
iqramujahid <iqra@myprintcloud.com>
2014-11-17 11:50:16 +0500

    iqra - Bundle config & scripts

--- MPC.Webstore/App_Start/BundleConfig.cs
+++ MPC.Webstore/App_Start/BundleConfig.cs
@@ -9,10 +9,20 @@ namespace MPC.Webstore
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.10.2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Slider").Include(
+                       "~/Scripts/js-image-slider.js",
+                       "~/Scripts/zoomSlider.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/input.watermark.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+                     "~/Scripts/toastr.js",
+                     "~/Scripts/toastr.min.js"
+                     ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
