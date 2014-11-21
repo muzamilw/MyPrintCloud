396a195b6f311fadc536bab8fd3dc9d3e34fe054
Khurram <khurram@innostark.com>
2014-11-21 11:51:32 +0500

    1. UserIdentityModel (Will be contain user info, access rights, roles)

---
+++ Models/Common/UserIdentityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Models.Common
+{
+    /// <summary>
+    /// User Identity Model
+    /// </summary>
+    public class UserIdentityModel
+    {
+    }
+}
