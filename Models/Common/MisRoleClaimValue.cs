19c6b395dcf0e5bdb8608d2ac738d11ae09bac85
zohaib-sikander <zohaibsikander@gmail.com>
2014-11-25 12:20:38 +0500

    changes zohaib

---
+++ Models/Common/MisRoleClaimValue.cs
@@ -0,0 +1,13 @@
+﻿namespace MPC.Models.Common
+{
+    /// <summary>
+    /// Role Claim Value
+    /// </summary>
+    public class MisRoleClaimValue
+    {
+        /// <summary>
+        /// Role
+        /// </summary>
+        public string Role { get; set; }
+    }
+}
