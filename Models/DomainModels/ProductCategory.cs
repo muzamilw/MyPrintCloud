2bb56a7de4f60a82c49e884d268b5eac7b5e4d62
rafiq-innostark <rafiq@innostark.com>
2014-12-10 14:13:15 +0500

    Remove select2 js package

--- Models/DomainModels/ProductCategory.cs
+++ Models/DomainModels/ProductCategory.cs
@@ -14,53 +14,53 @@ namespace MPC.Models.DomainModels
         public string Description1 { get; set; }
         public string Description2 { get; set; }
         public int LockedBy { get; set; }
-        public Nullable<long> CompanyId { get; set; }
-        public Nullable<int> ParentCategoryId { get; set; }
+        public long? CompanyId { get; set; }
+        public int? ParentCategoryId { get; set; }
         public int DisplayOrder { get; set; }
         public string ImagePath { get; set; }
         public string ThumbnailPath { get; set; }
-        public Nullable<bool> isEnabled { get; set; }
-        public Nullable<bool> isMarketPlace { get; set; }
+        public bool? isEnabled { get; set; }
+        public bool? isMarketPlace { get; set; }
         public string TemplateDesignerMappedCategoryName { get; set; }
-        public Nullable<bool> isArchived { get; set; }
-        public Nullable<bool> isPublished { get; set; }
-        public Nullable<double> TrimmedWidth { get; set; }
-        public Nullable<double> TrimmedHeight { get; set; }
-        public Nullable<bool> isColorImposition { get; set; }
-        public Nullable<bool> isOrderImposition { get; set; }
-        public Nullable<bool> isLinkToTemplates { get; set; }
-        public Nullable<int> Sides { get; set; }
-        public Nullable<bool> ApplySizeRestrictions { get; set; }
-        public Nullable<bool> ApplyFoldLines { get; set; }
-        public Nullable<double> WidthRestriction { get; set; }
-        public Nullable<double> HeightRestriction { get; set; }
-        public Nullable<int> CategoryTypeId { get; set; }
-        public Nullable<int> RegionId { get; set; }
-        public Nullable<decimal> ZoomFactor { get; set; }
-        public Nullable<decimal> ScaleFactor { get; set; }
-        public Nullable<bool> isShelfProductCategory { get; set; }
+        public bool? isArchived { get; set; }
+        public bool? isPublished { get; set; }
+        public double? TrimmedWidth { get; set; }
+        public double? TrimmedHeight { get; set; }
+        public bool? isColorImposition { get; set; }
+        public bool? isOrderImposition { get; set; }
+        public bool? isLinkToTemplates { get; set; }
+        public int? Sides { get; set; }
+        public bool? ApplySizeRestrictions { get; set; }
+        public bool? ApplyFoldLines { get; set; }
+        public double? WidthRestriction { get; set; }
+        public double? HeightRestriction { get; set; }
+        public int? CategoryTypeId { get; set; }
+        public int? RegionId { get; set; }
+        public decimal? ZoomFactor { get; set; }
+        public decimal? ScaleFactor { get; set; }
+        public bool? isShelfProductCategory { get; set; }
         public string MetaKeywords { get; set; }
         public string MetaDescription { get; set; }
         public string MetaTitle { get; set; }
-        public Nullable<long> OrganisationId { get; set; }
-        public Nullable<int> SubCategoryDisplayMode1 { get; set; }
-        public Nullable<int> SubCategoryDisplayMode2 { get; set; }
-        public Nullable<int> SubCategoryDisplayColumns { get; set; }
+        public long? OrganisationId { get; set; }
+        public int? SubCategoryDisplayMode1 { get; set; }
+        public int? SubCategoryDisplayMode2 { get; set; }
+        public int? SubCategoryDisplayColumns { get; set; }
         public string CategoryURLText { get; set; }
         public string MetaOverride { get; set; }
         public string ShortDescription { get; set; }
         public string SecondaryDescription { get; set; }
-        public Nullable<int> DefaultSortBy { get; set; }
-        public Nullable<int> ProductsDisplayColumns { get; set; }
-        public Nullable<int> ProductsDisplayRows { get; set; }
-        public Nullable<bool> IsDisplayFeaturedproducts { get; set; }
-        public Nullable<bool> IsShowAvailablity { get; set; }
-        public Nullable<bool> IsShowRewardPoints { get; set; }
-        public Nullable<bool> IsShowListPrice { get; set; }
-        public Nullable<bool> IsShowSalePrice { get; set; }
-        public Nullable<bool> IsShowStockStatus { get; set; }
-        public Nullable<bool> IsShowProductDescription { get; set; }
-        public Nullable<bool> IsShowProductShortDescription { get; set; }
+        public int? DefaultSortBy { get; set; }
+        public int? ProductsDisplayColumns { get; set; }
+        public int? ProductsDisplayRows { get; set; }
+        public bool? IsDisplayFeaturedproducts { get; set; }
+        public bool? IsShowAvailablity { get; set; }
+        public bool? IsShowRewardPoints { get; set; }
+        public bool? IsShowListPrice { get; set; }
+        public bool? IsShowSalePrice { get; set; }
+        public bool? IsShowStockStatus { get; set; }
+        public bool? IsShowProductDescription { get; set; }
+        public bool? IsShowProductShortDescription { get; set; }
 
         public virtual Company Company { get; set; }
     }
