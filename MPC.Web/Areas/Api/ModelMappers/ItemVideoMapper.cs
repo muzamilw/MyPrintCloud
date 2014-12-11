bd6f26b3ced7d19eb4939467a36d0f2cf8cdfb76
Khurram <khurram@innostark.com>
2014-12-11 11:48:14 +0500

    Products Screen Changes lost (while yesterday's merge)

---
+++ MPC.Web/Areas/Api/ModelMappers/ItemVideoMapper.cs
@@ -0,0 +1,40 @@
+﻿using MPC.MIS.Areas.Api.Models;
+namespace MPC.MIS.Areas.Api.ModelMappers
+{
+    using DomainModels = MPC.Models.DomainModels;
+
+    /// <summary>
+    /// Item Vdp Price Mapper
+    /// </summary>
+    public static class ItemVideoMapper
+    {
+        /// <summary>
+        /// Crete From Domain Model
+        /// </summary>
+        public static ItemVideo CreateFrom(this DomainModels.ItemVideo source)
+        {
+            return new ItemVideo
+            {
+                VideoId = source.VideoId,
+                ItemId = source.ItemId,
+                VideoLink = source.VideoLink,
+                Caption = source.Caption
+            };
+        }
+
+        /// <summary>
+        /// Crete From WebApi Model
+        /// </summary>
+        public static DomainModels.ItemVideo CreateFrom(this ItemVideo source)
+        {
+            return new DomainModels.ItemVideo
+            {
+                VideoId = source.VideoId,
+                ItemId = source.ItemId,
+                VideoLink = source.VideoLink,
+                Caption = source.Caption
+            };
+        }
+
+    }
+}
---
+++ MPC.Web/Areas/Api/Models/SupplierBaseResponse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace MPC.MIS.Areas.Api.Models
+{
+    /// <summary>
+    /// Supplier Api Base Response
+    /// </summary>
+    public class SupplierBaseResponse
+    {
+        /// <summary>
+        /// Company Types
+        /// </summary>
+        public IEnumerable<CompanyType> CompanyTypes { get; set; }
+
+        /// <summary>
+        /// Flags
+        /// </summary>
+        public IEnumerable<SectionFlagDropDown> Flags { get; set; }
+
+        /// <summary>
+        ///Price Flags
+        /// </summary>
+        public IEnumerable<SectionFlagDropDown> PriceFlags { get; set; }
+
+        /// <summary>
+        /// Markups
+        /// </summary>
+        public IEnumerable<Markup> Markups { get; set; }
+
+        /// <summary>
+        /// Nominal Codes
+        /// </summary>
+        public IEnumerable<ChartOfAccount> NominalCodes { get; set; }
+
+        /// <summary>
+        /// Deafult Managers
+        /// </summary>
+        public IEnumerable<SystemUserDropDown> SystemUsers { get; set; }
+
+        /// <summary>
+        /// Registration Questions
+        /// </summary>
+        public IEnumerable<RegistrationQuestionDropDown> RegistrationQuestions { get; set; }
+    }
+}
