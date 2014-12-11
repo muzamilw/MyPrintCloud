54859d0047b0125ee71511e61503a26bea5548c9
Khurram <khurram@innostark.com>
2014-12-11 17:40:08 +0500

    Supplier Changes

---
+++ MPC.Web/Areas/Api/ModelMappers/TemplateMapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MPC.MIS.Areas.Api.Models;
+namespace MPC.MIS.Areas.Api.ModelMappers
+{
+    using DomainModels = MPC.Models.DomainModels;
+
+    /// <summary>
+    /// Template Mapper
+    /// </summary>
+    public static class TemplateMapper
+    {
+        /// <summary>
+        /// Crete From Domain Model
+        /// </summary>
+        public static Template CreateFrom(this DomainModels.Template source)
+        {
+            return new Template
+            {
+                ProductId = source.ProductId,
+                ProductName = source.ProductName,
+                PdfTemplateHeight = source.PDFTemplateHeight,
+                PdfTemplateWidth = source.PDFTemplateWidth,
+                TemplatePages = source.TemplatePages != null ? source.TemplatePages.Select(vdp => vdp.CreateFrom()) : new List<TemplatePage>()
+            };
+        }
+
+        /// <summary>
+        /// Crete From WebApi Model
+        /// </summary>
+        public static DomainModels.Template CreateFrom(this Template source)
+        {
+            return new DomainModels.Template
+            {
+                ProductId = source.ProductId,
+                ProductName = source.ProductName,
+                PDFTemplateHeight = source.PdfTemplateHeight,
+                PDFTemplateWidth = source.PdfTemplateWidth,
+                TemplatePages = source.TemplatePages != null ? source.TemplatePages.Select(vdp => vdp.CreateFrom()).ToList() : new List<DomainModels.TemplatePage>()
+            };
+        }
+    }
+}
---
+++ MPC.Web/Areas/Api/ModelMappers/TemplatePageMapper.cs
@@ -0,0 +1,44 @@
+﻿using MPC.MIS.Areas.Api.Models;
+namespace MPC.MIS.Areas.Api.ModelMappers
+{
+    using DomainModels = MPC.Models.DomainModels;
+
+    /// <summary>
+    /// Template Page Mapper
+    /// </summary>
+    public static class TemplatePageMapper
+    {
+        /// <summary>
+        /// Crete From Domain Model
+        /// </summary>
+        public static TemplatePage CreateFrom(this DomainModels.TemplatePage source)
+        {
+            return new TemplatePage
+            {
+                ProductPageId = source.ProductPageId,
+                ProductId = source.ProductId,
+                Width = source.Width,
+                Height = source.Height,
+                PageName = source.PageName,
+                PageNo = source.PageNo
+            };
+        }
+
+        /// <summary>
+        /// Crete From WebApi Model
+        /// </summary>
+        public static DomainModels.TemplatePage CreateFrom(this TemplatePage source)
+        {
+            return new DomainModels.TemplatePage
+            {
+                ProductPageId = source.ProductPageId,
+                ProductId = source.ProductId,
+                Width = source.Width,
+                Height = source.Height,
+                PageName = source.PageName,
+                PageNo = source.PageNo
+            };
+        }
+
+    }
+}
