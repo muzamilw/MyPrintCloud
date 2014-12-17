83efdef98a916bbffc0651968d79da3cfaed2593
zohaib-sikander <zohaibsikander@gmail.com>
2014-12-17 14:14:36 +0500

    category Changes

--- Interfaces/WebStoreServices/ICompanyService.cs
+++ Interfaces/WebStoreServices/ICompanyService.cs
@@ -3,6 +3,7 @@ using MPC.Models.DomainModels;
 using MPC.Models.RequestModels;
 using MPC.Models.ResponseModels;
 using System;
+using MPC.Models.Common;
 
 namespace MPC.Interfaces.WebStoreServices
 {
@@ -45,5 +46,15 @@ namespace MPC.Interfaces.WebStoreServices
         List<ProductCategory> GetAllParentCorporateCatalog(int customerId);
 
         List<ProductCategory> GetParentCategories();
+
+        ProductCategory GetCategoryById(int categoryId);
+
+        List<ProductCategory> GetChildCategories(int categoryId);
+
+        List<ProductCategory> GetAllChildCorporateCatalogByTerritory(int customerId, int ContactId, int ParentCatId);
+
+        string[] CreatePageMetaTags(string MetaTitle, string metaDesc, string metaKeyword, StoreMode mode,string StoreName, Address address = null);
+
+        Address GetDefaultAddressByStoreID(Int64 StoreID);
     }
 }
