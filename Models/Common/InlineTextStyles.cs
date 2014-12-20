104020672df3b1ec7f9ecc4b7fb6fecd23dd9063
zohaib-sikander <zohaibsikander@gmail.com>
2014-12-20 09:57:10 +0500

    changes

---
+++ Models/Common/InlineTextStyles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Models.Common
+{
+    public class InlineTextStyles
+    {
+        public string textColor { get; set; }
+        public string fontName { get; set; }
+        public string fontSize { get; set; }
+        public string fontWeight { get; set; }
+        public string fontStyle { get; set; }
+        public string characterIndex { get; set; }
+        public string textCMYK { get; set; }
+    }
+}
