0630d30a216c440b14c4e888d5c172705306e13e
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-01-01 14:28:29 +0500

    Designer Api --Area  and template repository added by saqib

---
+++ Implementation/WebStoreServices/TemplateService.cs
@@ -0,0 +1,38 @@
+﻿using MPC.Interfaces.Repository;
+using MPC.Interfaces.WebStoreServices;
+using MPC.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Implementation.WebStoreServices
+{
+    class TemplateService : ITemplateService
+    {
+        #region private
+        public readonly ITemplateRepository _templateRepository;
+        #endregion
+        #region constructor
+        public TemplateService(ITemplateRepository templateRepository)
+        {
+            this._templateRepository = templateRepository;
+        }
+        #endregion
+
+        #region public
+        public Template GetTemplate(int productID)
+        {
+            var product= _templateRepository.GetTemplate(productID);
+            if (product.Orientation == 2) //rotating the canvas in case of vert orientation
+            {
+                double tmp = product.PDFTemplateHeight.Value;
+                product.PDFTemplateHeight = product.PDFTemplateWidth;
+                product.PDFTemplateWidth = tmp;
+            }
+            return product;
+        }
+        #endregion
+    }
+}
