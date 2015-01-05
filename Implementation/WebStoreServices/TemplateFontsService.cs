07a6a08bc632d04ae2ccb04e22b3124a6b39bb67
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-01-05 15:08:49 +0500

    designer Fonts service

---
+++ Implementation/WebStoreServices/TemplateFontsService.cs
@@ -0,0 +1,54 @@
+﻿using MPC.Interfaces.Repository;
+using MPC.Interfaces.WebStoreServices;
+using MPC.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Implementation.WebStoreServices
+{
+    class TemplateFontsService : ITemplateFontsService
+    {
+        #region private
+        public readonly ITemplateFontsRepository _templateFontRepository;
+        #endregion
+        #region constructor
+        public TemplateFontsService(ITemplateFontsRepository templateFontRepository)
+        {
+            this._templateFontRepository = templateFontRepository;
+        }
+        #endregion
+
+        #region public
+       
+        public List<TemplateFont> GetFontList(int productId, int customerId)
+        {
+            var fonts = _templateFontRepository.GetFontList(productId, customerId);
+            List<TemplateFont> objToReturn = new List<TemplateFont>();
+            foreach (var objFonts in fonts)
+            {
+                string path = "";
+                if (objFonts.FontPath != null)
+                {
+                    path = objFonts.FontPath;
+                }
+                else
+                {
+                    path = "PrivateFonts/FontFace/";
+                }
+
+                objFonts.FontFile = "Designer/" + path + objFonts.FontFile;
+                objToReturn.Add(objFonts);
+            }
+            return objToReturn;
+        }
+
+       
+
+     
+
+        #endregion
+    }
+}
---
+++ MPC.Webstore/Areas/DesignerApi/Controllers/TemplateFontsController.cs
@@ -0,0 +1,43 @@
+﻿using MPC.Interfaces.WebStoreServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace MPC.Webstore.Areas.DesignerApi.Controllers
+{
+    public class TemplateFontsController : ApiController
+    {
+       #region Private
+       private readonly ITemplateFontsService templateFontSvc;
+       #endregion
+       #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="companyService"></param>
+        public TemplateFontsController(ITemplateFontsService templateFontSvc)
+        {
+            this.templateFontSvc = templateFontSvc;
+        }
+
+        #endregion
+        #region public
+        public HttpResponseMessage GetFontList(int parameter1, int parameter2)
+        {
+            var template = templateFontSvc.GetFontList(parameter1,parameter2);
+            var formatter = new JsonMediaTypeFormatter();
+            var json = formatter.SerializerSettings;
+            json.Formatting = Newtonsoft.Json.Formatting.Indented;
+            json.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            return Request.CreateResponse(HttpStatusCode.OK, template, formatter);
+
+        }
+        #endregion
+    }
+}
