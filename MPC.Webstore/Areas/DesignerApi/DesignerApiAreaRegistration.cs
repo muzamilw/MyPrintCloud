96daf626723191b75992fe7c61fedfa9427e3b95
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-01-08 10:29:54 +0500

    designer integration

--- MPC.Webstore/Areas/DesignerApi/DesignerApiAreaRegistration.cs
+++ MPC.Webstore/Areas/DesignerApi/DesignerApiAreaRegistration.cs
@@ -21,7 +21,7 @@ namespace MPC.Webstore.Areas.DesignerApi
                defaults: new { id = RouteParameter.Optional }
             );
             context.Routes.MapHttpRoute(
-             name: "DesignerApiDefault_2params",
+             name: "DesignerApiDefault_MultiParams",
              routeTemplate: AreaName + "/{controller}/{action}/{parameter1}/{parameter2}/{parameter3}/{parameter4}/{parameter5}/{parameter6}",
              defaults: new { parameter1 = RouteParameter.Optional, parameter2 = RouteParameter.Optional, parameter3 = RouteParameter.Optional, parameter4 = RouteParameter.Optional, parameter5 = RouteParameter.Optional,parameter6 = RouteParameter.Optional }
           );
