9c4c96594c58064d3abd2af67fa39aa0882b51dd
rafiq-innostark <rafiq@innostark.com>
2015-01-15 13:35:38 +0500

    Phrase Library complete

--- MPC.Web/Areas/Common/Scripts/phraseLibrary.viewModel.js
+++ MPC.Web/Areas/Common/Scripts/phraseLibrary.viewModel.js
@@ -12,10 +12,16 @@ define("common/phraseLibrary.viewModel",
                     selectedSection = ko.observable(),
                     //select Phrase Field
                     selectedPhraseField = ko.observable(),
+                    //Flag for open from Phrase Library
+                    isOpenFromPhraseLibrary = ko.observable(false),
+                    //selected Phrase
+                    selectedPhrase = ko.observable(false),
                     //Sections
                     sections = ko.observableArray([]),
                     //Phrase List
                     phrases = ko.observableArray([]),
+                    //job Titles List
+                    jobTitles = ko.observableArray([]),
                     //#endregion
                     //get All Sections
                     getAllSections = function () {
@@ -108,92 +114,160 @@ define("common/phraseLibrary.viewModel",
                             }
                         });
                     },
-                  //select Section(Category)
-                  selectSection = function (section) {
-                      //old menu collapse
-                      if (selectedSection() !== undefined) {
-                          selectedSection().isExpanded(false);
-                      }
-                      //new selected section expand
-                      section.isExpanded(true);
-                      selectedSection(section);
-                      selectedPhraseField(undefined);
-                  },
+                   //select Section(Category)
+                   selectSection = function (section) {
+                       //old menu collapse
+                       if (selectedSection() !== undefined) {
+                           selectedSection().isExpanded(false);
+                       }
+                       //new selected section expand
+                       section.isExpanded(true);
+                       selectedSection(section);
+                       selectedPhraseField(undefined);
+                       phrases.removeAll();
+                   },
                    //select Phrase Field
-                  selectPhraseField = function (phraseField) {
-                      phrases.removeAll();
-                      selectedPhraseField(phraseField);
-                      if (phraseField.phrases().length > 0) {
-                          ko.utils.arrayPushAll(phrases, phraseField.phrases());
-                          phrases.valueHasMutated();
-                      }
-                      else {
-                          getPhrasesByPhraseFieldId(phraseField.fieldId());
-                      }
-                  },
-                  //Delete Phrase
-                  deletePhrase = function (phrase) {
-                      phrase.isDeleted(true);
-                  },
-                  //Save Phrase Library
-                  savePhraseLibrary = function (phraseLibrary) {
-                      var phraseLibrarySaveModel = model.PhraseLibrarySaveModel();
-                      var severModel = phraseLibrarySaveModel.convertToServerData(phraseLibrarySaveModel);
-                      _.each(sections(), function (item) {
-                          var section = item.convertToServerData(item);
-                          _.each(item.phrasesFields(), function (phraseFiledItem) {
-                              var phraseField = phraseFiledItem.convertToServerData(phraseFiledItem);
-                              if (phraseFiledItem.phrases().length > 0) {
-                                  _.each(phraseFiledItem.phrases(), function (phraseItem) {
-                                      if (phraseItem.hasChanges()) {
-                                          phraseField.Phrases.push(phraseItem.convertToServerData(phraseItem));
-                                      }
-                                  });
-                              }
-                              section.PhrasesFields.push(phraseField);
-                          });
-                          severModel.Sections.push(section);
-                      });
-
-                      dataservice.savePhaseLibrary(
-                               severModel, {
+                   selectPhraseField = function (phraseField) {
+                       phrases.removeAll();
+                       //If open from other than phase library secreen like Product
+                       if (!isOpenFromPhraseLibrary()) {
+                           if (phraseField.sectionId() === 4) {
+                               selectedPhraseField(phraseField);
+                               if (phraseField.phrases().length > 0) {
+                                   ko.utils.arrayPushAll(phrases, phraseField.phrases());
+                                   phrases.valueHasMutated();
+                               } else {
+                                   getPhrasesByPhraseFieldId(phraseField.fieldId());
+                               }
+                           }
+                       }
+                           //If Open From Phase Library Screen
+                       else {
+                           selectedPhraseField(phraseField);
+                           if (phraseField.phrases().length > 0) {
+                               ko.utils.arrayPushAll(phrases, phraseField.phrases());
+                               phrases.valueHasMutated();
+                           } else {
+                               getPhrasesByPhraseFieldId(phraseField.fieldId());
+                           }
+                       }
+                   },
+                   //Delete Phrase
+                   deletePhrase = function (phrase) {
+                       phrase.isDeleted(true);
+                   },
+                   //Save Phrase Library
+                   savePhraseLibrary = function (phraseLibrary) {
+                       var phraseLibrarySaveModel = model.PhraseLibrarySaveModel();
+                       var severModel = phraseLibrarySaveModel.convertToServerData(phraseLibrarySaveModel);
+                       _.each(sections(), function (item) {
+                           if (item.phrasesFields().length > 0) {
+                               var section = item.convertToServerData(item);
+                               _.each(item.phrasesFields(), function (phraseFiledItem) {
+                                   var phraseField = phraseFiledItem.convertToServerData(phraseFiledItem);
+                                   if (phraseFiledItem.phrases().length > 0) {
+                                       _.each(phraseFiledItem.phrases(), function (phraseItem) {
+                                           if (phraseItem.hasChanges()) {
+                                               phraseField.Phrases.push(phraseItem.convertToServerData(phraseItem));
+                                           }
+                                       });
+                                   }
+                                   section.PhrasesFields.push(phraseField);
+                               });
+                               severModel.Sections.push(section);
+                           }
+                       });
+                       saveLibrary(severModel);
+                   },
+                   //
+                   saveLibrary = function (phaseLibrary) {
+                       dataservice.savePhaseLibrary(
+                               phaseLibrary, {
                                    success: function (data) {
-                                       view.hidePhraseLibraryDialog();
+                                       //view.hidePhraseLibraryDialog();
                                        toastr.success("Successfully save.");
                                    },
                                    error: function (response) {
                                        toastr.error("Failed to Save . Error: " + response);
                                    }
                                });
-                  },
-                  //Add Phrase
-                  addPhrase = function () {
-                      if (selectedPhraseField() != undefined) {
-                          selectedPhraseField().phrases.splice(0, 0, model.Phrase(0, "", selectedPhraseField().fieldId()));
-                          phrases.splice(0, 0, selectedPhraseField().phrases()[0]);
-                      }
-                  },
-                  editJobTitle = function () {
-                      if (selectedSection() !== undefined && selectedSection().phrasesFields().length > 0) {
-                          view.showEditJobTitleModalDialog();
-                      }
-                  },
-                   anotherObservableArray = ko.observableArray([
-    { name: "Bungle", type: "Bear" },
-    { name: "George", type: "Hippo" },
-    { name: "Zippy", type: "Unknown" }
-                   ]),
-                // Initialize the view model
-                initialize = function (specifiedView) {
-                    view = specifiedView;
-                    ko.applyBindings(view.viewModel, view.bindingRoot);
-                    getAllSections();
-                    view.showPhraseLibraryDialog();
-                };
+                   },
+                   //Add Phrase
+                   addPhrase = function () {
+                       if (selectedPhraseField() != undefined) {
+                           selectedPhraseField().phrases.splice(0, 0, model.Phrase(0, "", selectedPhraseField().fieldId()));
+                           phrases.splice(0, 0, selectedPhraseField().phrases()[0]);
+                       }
+                   },
+                   //Edit Job Title (open edit job title dialog)
+                   editJobTitle = function () {
+                       jobTitles.removeAll();
+                       _.each(sections(), function (section) {
+                           if (section.sectionId() === 4) {
+                               ko.utils.arrayPushAll(jobTitles(), section.phrasesFields());
+                               jobTitles.valueHasMutated();
+                           }
+                       });
+                       view.showEditJobTitleModalDialog();
+                   },
+                   //Close Edit job Title Dialog
+                   closeEditJobDialog = function () {
+                       view.hideEditJobTitleDialog();
+                   },
+                   //Template To Use
+                    templateToUse = function () {
+                        if (isOpenFromPhraseLibrary()) {
+                            return 'phraseEditItemTemplate';
+                        } else {
+                            return 'phraseItemTemplate';
+                        }
+                    },
+                    //Select Phrase
+                    selectPhrase = function (phrase) {
+                        if (phrase.isPhraseChecked()) {
+                            view.hidePhraseLibraryDialog();
+                            var phraseLibrarySaveModel = model.PhraseLibrarySaveModel();
+                            var severModel = phraseLibrarySaveModel.convertToServerData(phraseLibrarySaveModel);
+                            _.each(sections(), function (item) {
+                                if (item.sectionId() === 4) {
+                                    var section = item.convertToServerData(item);
+                                    _.each(item.phrasesFields(), function (phraseFiledItem) {
+                                        var phraseField = phraseFiledItem.convertToServerData(phraseFiledItem);
+                                        if (phraseFiledItem.phrases().length > 0) {
+                                            _.each(phraseFiledItem.phrases(), function (phraseItem) {
+                                                if (phraseItem.hasChanges()) {
+                                                    phraseField.Phrases.push(phraseItem.convertToServerData(phraseItem));
+                                                }
+                                            });
+                                        }
+                                        if (phraseFiledItem.hasChanges() || phraseField.Phrases.length > 0) {
+                                            section.PhrasesFields.push(phraseField);
+                                        }
+                                    });
+                                    if (section.PhrasesFields.length > 0)
+                                        severModel.Sections.push(section);
+                                }
+                            });
+
+                            if (severModel.Sections.length > 0) {
+                                saveLibrary(severModel);
+                            }
+                        }
+                    },
+                   // Initialize the view model
+                   initialize = function (specifiedView) {
+                       view = specifiedView;
+                       selectedSection(model.Section());
+                       ko.applyBindings(view.viewModel, view.bindingRoot);
+                       getAllSections();
+                       view.showPhraseLibraryDialog();
+                   };
 
                 return {
                     selectedSection: selectedSection,
                     selectedPhraseField: selectedPhraseField,
+                    isOpenFromPhraseLibrary: isOpenFromPhraseLibrary,
+                    selectedPhrase: selectedPhrase,
                     //Arrays
                     sections: sections,
                     phrases: phrases,
@@ -205,7 +279,10 @@ define("common/phraseLibrary.viewModel",
                     savePhraseLibrary: savePhraseLibrary,
                     addPhrase: addPhrase,
                     editJobTitle: editJobTitle,
-                    anotherObservableArray: anotherObservableArray,
+                    closeEditJobDialog: closeEditJobDialog,
+                    templateToUse: templateToUse,
+                    selectPhrase: selectPhrase,
+                    jobTitles: jobTitles,
                 };
             })()
         };
