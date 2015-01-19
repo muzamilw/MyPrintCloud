da9b75d3cb3319d2b60498992a1ae94db306d00d
muzamilw <muzamilw@hotmail.com>
2015-01-19 20:16:39 +0500

    bb

--- MPC.Provisioning/Scripts/provisionNew.ps1
+++ MPC.Provisioning/Scripts/provisionNew.ps1
@@ -32,25 +32,36 @@ if (!(Test-Path $siteName -pathType container))
 #navigate to the sites root
 cd IIS:\Sites\
 
+$virtualDirectoryName = "mpc-content"
+$virtualDirectoryPath = "IIS:\Sites\$siteName\$virtualDirectoryName"
+
 #check if the site exists
 if (Test-Path $siteName -pathType container)
 {
+	if(!(Test-Path -Path $virtualDirectoryPath )){
+		# this command will create virtual folder
+		New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $sitePhysicalPath
+	}
     return "App already exists"
 }
 
 #create the site
 $iisApp = New-Item $siteName -bindings @{protocol="http";bindingInformation=":80:" + $siteName} -physicalPath $sitePhysicalPath
 $iisApp | Set-ItemProperty -Name "applicationPool" -Value $siteName
-$virtualDirectoryName = "mpc-content"
 
-$virtualDirectoryPath = "IIS:\Sites\$siteName\$virtualDirectoryName"
 
-#New-WebApplication -Name $siteName -ApplicationPool "applicationPool" -PhysicalPath $sitePhysicalPath -Site $virtualDirectoryName
 
-New-WebApplication "visrta" -Site $siteName -ApplicationPool "applicationPools" -
-PhysicalPath $sitePhysicalPath
-#New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $sitePhysicalPath
-# Init
-#IIS:\>New-WebVirtualDirectory -Site $siteName -Name "mpc-content" -PhysicalPath "E:\Development"
 
+#this command will create web app
+New-WebApplication -Name mis -Site $siteName -PhysicalPath $sitePhysicalPath -ApplicationPool $appPool
+
+# this command will create virtual folder
+New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $sitePhysicalPath
+
+$resourceFileDirectory = "IIS:\Sites\$siteName\$virtualDirectoryPath"
+
+#New-Item $resourceFileDirectory -type directory -physicalPath $sitePhysicalPath
+#New-Item -ItemType directory -Path $resourceFileDirectory
+#New-Item -ItemType Directory -Path $resourceFileDirectory -Force
+New-Item -Path "$resourceFileDirectory"  -ItemType Directory 
 return "App Created" + $siteName
