8f377b882a7cde8304d22cf912d7fef918a24f2e
Khurram <khurram@innostark.com>
2015-01-19 14:56:58 +0500

    Company, StockItem relationship added.

--- MPC.Web/Areas/Api/Models/StockItem.cs
+++ MPC.Web/Areas/Api/Models/StockItem.cs
@@ -73,7 +73,7 @@ namespace MPC.MIS.Areas.Api.Models
         /// <summary>
         /// Supplier Id
         /// </summary>
-        public int? SupplierId { get; set; }
+        public long? SupplierId { get; set; }
 
         /// <summary>
         /// Cost Price
