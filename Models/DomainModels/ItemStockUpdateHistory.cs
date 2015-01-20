091bce554272eb5c079320d6b3fc18df790af714
Khurram <khurram@innostark.com>
2015-01-20 18:51:01 +0500

    1. ItemStockUpdateHistory added to Edmx 2. isCalculateTaxByService field added to Company 3. isAddCropMarks field added to Items

---
+++ Models/DomainModels/ItemStockUpdateHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MPC.Models.DomainModels
+{
+    /// <summary>
+    /// Item Stock Update History Domain Model
+    /// </summary>
+    public class ItemStockUpdateHistory
+    {
+        public int StockHistoryId { get; set; }
+        public int? ItemId { get; set; }
+        public int? LastAvailableQty { get; set; }
+        public int? LastModifiedQty { get; set; }
+        public int? LastOrderedQty { get; set; }
+        public int? ModifyEvent { get; set; }
+        public int? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public int? OrderID { get; set; }
+    }
+}
