63d4b581d32625b4cdaa9c5be7965e76df4f9095
muzamilw <muzamilw@hotmail.com>
2015-01-20 18:44:33 +0500

    checkin

--- MPC.Provisioning/Scripts/provisionNew.ps1
+++ MPC.Provisioning/Scripts/provisionNew.ps1
@@ -4,19 +4,23 @@ param(
     $siteName,
 
     [parameter(Mandatory = $true)]
-    $sitePhysicalPath
+    $sitePhysicalPath,
 
-    
+	[parameter(Mandatory = $true)]
+    $siteOrganisationId
 
 )
+
 
 
 Set-ExecutionPolicy Bypass -Scope Process
 Import-Module WebAdministration
 
+return $sitePhysicalPath
+
 $iisAppPoolDotNetVersion = "v4.0"
 
-
+$mpcContentVirtualPath = "e:\mpc-content"
 
 #navigate to the app pools root
 cd IIS:\AppPools\
@@ -34,14 +38,62 @@ cd IIS:\Sites\
 
 $virtualDirectoryName = "mpc-content"
 $virtualDirectoryPath = "IIS:\Sites\$siteName\$virtualDirectoryName"
+$resourceFileDirectory = "$mpcContentVirtualPath\Resources"
+$OrganisationName = "Organisation" + $siteOrganisationId
+$resourceFileDirectoryWithOrganisation = "$resourceFileDirectory\$OrganisationName"
+$CopyResourceFolderPath = "$resourceFileDirectoryWithOrganisation\"
 
 #check if the site exists
 if (Test-Path $siteName -pathType container)
 {
-	if(!(Test-Path -Path $virtualDirectoryPath )){
+	if(!(Test-Path -Path $mpcContentVirtualPath )){
 		# this command will create virtual folder
-		New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $sitePhysicalPath
+		New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $mpcContentVirtualPath
 	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Artworks" )){
+		New-Item "$mpcContentVirtualPath\Artworks" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Attachments" )){
+		New-Item "$mpcContentVirtualPath\Attachments" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Categories" )){
+		New-Item "$mpcContentVirtualPath\Categories" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\CompanyBanners" )){
+		New-Item "$mpcContentVirtualPath\CompanyBanners" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\CostCenres" )){
+		New-Item "$mpcContentVirtualPath\CostCenres" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Designer" )){
+		New-Item "$mpcContentVirtualPath\Designer" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Media" )){
+		New-Item "$mpcContentVirtualPath\Media" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Organisations" )){
+		New-Item "$mpcContentVirtualPath\Organisations" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Products" )){
+		New-Item "$mpcContentVirtualPath\Products" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\SecondaryPages" )){
+		New-Item "$mpcContentVirtualPath\SecondaryPages" -type directory
+	}
+	if(!(Test-Path -Path "$mpcContentVirtualPath\Stores" )){
+		New-Item "$mpcContentVirtualPath\Stores" -type directory
+	}
+	if(!(Test-Path -Path $resourceFileDirectory ))
+	{
+		New-Item $resourceFileDirectory -type directory
+		if(!(Test-Path -Path $resourceFileDirectoryWithOrganisation )){
+		New-Item $resourceFileDirectoryWithOrganisation -type directory
+		}
+	}
+	else if(!(Test-Path -Path $resourceFileDirectoryWithOrganisation )){
+		New-Item $resourceFileDirectoryWithOrganisation -type directory
+	}
+	
     return "App already exists"
 }
 
@@ -56,12 +108,32 @@ $iisApp | Set-ItemProperty -Name "applicationPool" -Value $siteName
 New-WebApplication -Name mis -Site $siteName -PhysicalPath $sitePhysicalPath -ApplicationPool $appPool
 
 # this command will create virtual folder
-New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $sitePhysicalPath
+New-Item $virtualDirectoryPath -type VirtualDirectory -physicalPath $mpcContentVirtualPath
 
-$resourceFileDirectory = "IIS:\Sites\$siteName\$virtualDirectoryPath"
 
-#New-Item $resourceFileDirectory -type directory -physicalPath $sitePhysicalPath
-#New-Item -ItemType directory -Path $resourceFileDirectory
-#New-Item -ItemType Directory -Path $resourceFileDirectory -Force
-New-Item -Path "$resourceFileDirectory"  -ItemType Directory 
-return "App Created" + $siteName
+# create resource directory
+New-Item $resourceFileDirectory -type directory
+
+#create all other directories
+New-Item "$mpcContentVirtualPath\Artworks" -type directory
+New-Item "$mpcContentVirtualPath\Attachments" -type directory
+New-Item "$mpcContentVirtualPath\Categories" -type directory
+New-Item "$mpcContentVirtualPath\CompanyBanners" -type directory
+New-Item "$mpcContentVirtualPath\CostCenres" -type directory
+New-Item "$mpcContentVirtualPath\Designer" -type directory
+New-Item "$mpcContentVirtualPath\Media" -type directory
+New-Item "$mpcContentVirtualPath\Organisations" -type directory
+New-Item "$mpcContentVirtualPath\Products" -type directory
+New-Item "$mpcContentVirtualPath\SecondaryPages" -type directory
+New-Item "$mpcContentVirtualPath\Stores" -type directory
+
+#create resource file folder with organisation
+New-Item $resourceFileDirectoryWithOrganisation -type directory
+
+#copies the english folder
+Copy-Item -Path E:\Development\MyPrintCloud\MyPrintCloud.Cloud\MyPrintCloud\MPC.Web\MPC_Content\Resources\Organisation1\en-Us -Destination $CopyResourceFolderPath -recurse -Force
+
+#copies the french folder
+Copy-Item -Path E:\Development\MyPrintCloud\MyPrintCloud.Cloud\MyPrintCloud\MPC.Web\MPC_Content\Resources\Organisation1\fr-FR -Destination $CopyResourceFolderPath -recurse -Force
+
+return "App Created"
