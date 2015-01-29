50192dc23ffb4a68ba58ba709ce5fb77c22e1887
khurram <khurram@innostark.com>
2015-01-29 11:26:56 +0500

    Product: Press Selection (dialog for Page Detail & Section)

---
+++ MPC.Web/Areas/Api/Models/Machine.cs
@@ -0,0 +1,18 @@
+﻿namespace MPC.MIS.Areas.Api.Models
+{
+    /// <summary>
+    /// Machine Web Api Model
+    /// </summary>
+    public class Machine
+    {
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+        public int? MachineCatId { get; set; }
+        public int DefaultPaperId { get; set; }
+        public double? Maximumsheetweight { get; set; }
+        public double? Maximumsheetheight { get; set; }
+        public double? Maximumsheetwidth { get; set; }
+        public double? Minimumsheetheight { get; set; }
+        public double? Minimumsheetwidth { get; set; }
+    }
+}
