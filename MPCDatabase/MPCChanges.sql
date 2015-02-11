4fbb75f56b7a73a4d5f07de3bbfe07b3186479bc
khurram <khurram@innostark.com>
2015-02-11 16:42:27 +0500

    DbChanges

--- MPCDatabase/MPCChanges.sql
+++ MPCDatabase/MPCChanges.sql
@@ -35,6 +35,14 @@ alter table Activity
 add foreign key (CompanyId)
 references Company(CompanyId)
 
+drop index Activity.SystemUserID
+
+alter table activity
+drop constraint DF__tbl_activ__Syste__0DAF0CB0
+
+alter table activity
+alter column SystemUserId uniqueidentifier
+
 exec sp_rename 'Invoice.ContactCompanyId', 'CompanyId', 'Column'
 
 alter table Invoice
@@ -51,5 +59,12 @@ alter column EstimateId bigint
 
 alter table Estimate
 drop column companyname
+
+alter table company
+drop constraint DF__tbl_custo__Accou__30C33EC3
+
+alter table company
+alter column accountmanagerid uniqueidentifier null
+
 
 GO
