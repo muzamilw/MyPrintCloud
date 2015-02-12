dc9e0d2e2bc39003d8eea294352376678439a80c
msarosh786 <msarosh@live.com>
2015-02-12 12:30:55 +0500

    InvoiceDetail.cshtml UI Settings

---
+++ MPC.Web/Areas/DeliveryNotes/DeliveryNoteAreaRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace MPC.MIS.Areas.Orders
+{
+    /// <summary>
+    /// Orders Area Registration
+    /// </summary>
+    public class DeliveryNoteAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Delivery Notes";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Orders_default",
+                "DeliveryNotes/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "MPC.MIS.Areas.DeliveryNotes.Controllers" }
+            );
+        }
+    }
+}
