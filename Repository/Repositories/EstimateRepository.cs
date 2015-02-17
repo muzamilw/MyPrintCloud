ab5867e16acfdfb0298b6edbf0105dfc5a45d94e
Baqer Naqvi <link2naqvi@gmail.com>
2015-02-17 17:14:08 +0500

    fixes in EstimateRepository

--- Repository/Repositories/EstimateRepository.cs
+++ Repository/Repositories/EstimateRepository.cs
@@ -103,17 +103,15 @@ namespace MPC.Repository.Repositories
         /// </summary>
         public OrderStatusesResponse GetOrderStatusesCount()
         {
-            List<Estimate> allEstimates = DbSet.ToList();
-            List<Estimate> orders = allEstimates.Where(estimate => estimate.isEstimate == false).ToList();
-            List<Estimate> unConfirmedOrders = allEstimates.Where(estimate => estimate.isEstimate == true).ToList();
             return new OrderStatusesResponse
             {
-                PendingOrdersCount = orders.Count(order => order.StatusId == (short)OrderStatusEnum.PendingOrder),
-                InProductionOrdersCount = orders.Count(order => order.StatusId == (short)OrderStatusEnum.InProduction),
-                CompletedOrdersCount = orders.Count(order => order.StatusId == (short)OrderStatusEnum.CompletedOrders),
-                UnConfirmedOrdersCount = unConfirmedOrders.Count,
-                TotalEarnings = DbSet.Sum(estimate => estimate.Estimate_Total),
-                CurrentMonthOdersCount = orders.Count(order =>order.Order_Date.HasValue && DateTime.Now.Month == order.Order_Date.Value.Month)
+                PendingOrdersCount = DbSet.Count(order => order.StatusId == (short)OrderStatusEnum.PendingOrder && order.isEstimate == false),
+                InProductionOrdersCount = DbSet.Count(order => order.StatusId == (short)OrderStatusEnum.InProduction && order.isEstimate == false),
+                CompletedOrdersCount = DbSet.Count(order => order.StatusId == (short)OrderStatusEnum.CompletedOrders && order.isEstimate == false),
+                UnConfirmedOrdersCount = DbSet.Count((estimate => estimate.isEstimate == true)),
+                TotalEarnings = DbSet.Sum(estimate => estimate.Estimate_Total ),
+                CurrentMonthOdersCount = DbSet.Count(order => order.Order_Date.HasValue && 
+                    order.isEstimate == false && DateTime.Now.Month == order.Order_Date.Value.Month)
             };
         }
 
