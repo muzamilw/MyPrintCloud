8cbd15e4024bd0846a3fbe2aa5f374761315b3b0
hadiabhoon <hadia@myprintcloud.com>
2015-02-18 17:46:25 +0500

    Delivery Cost Center List and  detail screen

--- MPC.Web/Areas/Api/Models/CostCentre.cs
+++ MPC.Web/Areas/Api/Models/CostCentre.cs
@@ -135,6 +135,7 @@ namespace MPC.MIS.Areas.Api.Models
         public bool? isFromMIS { get; set; }
         public string XeroAccessCode { get; set; }
         public long? OrganisationId { get; set; }
+        public string DeliveryServiceType { get; set; }
         public virtual ICollection<CostcentreInstruction> CostcentreInstructions { get; set; }
         public virtual ICollection<CostcentreResource> CostcentreResources { get; set; }
        
---
+++ MPC.Web/Areas/Api/Models/OrderBaseResponseForCompany.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MPC.MIS.Areas.Api.Models
+{
+    /// <summary>
+    /// Order Base Response For Company Web Api Model
+    /// </summary>
+    public class OrderBaseResponseForCompany
+    {
+        /// <summary>
+        /// Contacts
+        /// </summary>
+        public IEnumerable<CompanyContactDropDownForOrder> CompanyContacts { get; set; }
+
+        /// <summary>
+        /// Addresses
+        /// </summary>
+        public IEnumerable<AddressDropDown> CompanyAddresses { get; set; }
+    }
+}
