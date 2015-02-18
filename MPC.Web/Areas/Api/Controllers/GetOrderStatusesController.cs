e51c83e3fb71dba62d380d73c828efb1aae03149
hadiabhoon <hadia@myprintcloud.com>
2015-02-18 18:27:18 +0500

    changes

---
+++ MPC.Web/Areas/Api/Controllers/GetOrderStatusesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Web;
+using System.Web.Http;
+using MPC.Interfaces.MISServices;
+using MPC.MIS.Areas.Api.ModelMappers;
+using MPC.MIS.Areas.Api.Models;
+
+namespace MPC.MIS.Areas.Api.Controllers
+{
+    /// <summary>
+    /// Controller to Get Order's Statuses
+    /// </summary>
+    public class GetOrderStatusesController : ApiController
+    {
+        #region Private
+        private readonly IDashboardService   dashboardService;
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GetOrderStatusesController( IDashboardService dashboardService)
+        {
+            this.dashboardService = dashboardService;
+        }
+
+        #endregion
+        #region Public
+
+        /// <summary>
+        /// Get Addresses / Compnay Contacts
+        /// </summary>
+        public OrderStatusesResponse Get()
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpException((int)HttpStatusCode.BadRequest, "Invalid Request");
+            }
+            return dashboardService.GetOrderStatusesCount();
+        }
+        #endregion
+    }
+}
---
+++ Models/Common/IsTrialClaimValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MPC.Models.Common
+{
+    public class IsTrialClaimValue
+    {
+        /// <summary>
+        /// Defines if user is in trial period
+        /// </summary>
+        public Boolean IsTrial { get; set; }
+    }
+}
