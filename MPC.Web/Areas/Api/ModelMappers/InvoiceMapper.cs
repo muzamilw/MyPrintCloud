ff29516af894f46c27cfeb323425c84009376185
Baqer Naqvi <link2naqvi@gmail.com>
2015-02-23 17:22:04 +0500

    Invoice Tab work

---
+++ MPC.Web/Areas/Api/ModelMappers/InvoiceMapper.cs
@@ -0,0 +1,40 @@
+﻿using MPC.MIS.Areas.Api.Models;
+using System.Linq;
+using Invoice = MPC.Models.DomainModels.Invoice;
+
+namespace MPC.MIS.Areas.Api.ModelMappers
+{
+    public static class InvoiceMapper
+    {
+        /// <summary>
+        /// Domain response to Web response invoice mapper
+        /// </summary>
+        public static InvoiceRequestResponseModel CreateFrom(this MPC.Models.ResponseModels.InvoiceRequestResponseModel source)
+        {
+            return new InvoiceRequestResponseModel
+            {
+                RowCount = source.RowCount,
+                Invoices = source.Invoices.Select(invoice => invoice.CreateFrom())
+            };
+        }
+
+        /// <summary>
+        /// Domain invoice to web invoice mapper
+        /// </summary>
+        private static Models.Invoice CreateFrom(this Invoice source)
+        {
+            return new Models.Invoice
+            {
+                InvoiceId = source.InvoiceId,
+                CompanyName = source.Company.Name,
+                InvoiceCode = source.InvoiceCode,
+                InvoiceName = source.InvoiceName,
+                IsArchive = source.IsArchive,
+                InvoiceDate = source.InvoiceDate,
+                InvoiceTotal = source.InvoiceTotal
+                
+            };
+        }
+
+    }
+}
