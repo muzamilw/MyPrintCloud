c73f006240204465923628c5c51e5bcdb8d9c119
Baqer Naqvi <link2naqvi@gmail.com>
2015-02-24 15:47:10 +0500

    Image-name rework in item,company,organization service

---
+++ Models/Common/StringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace MPC.Models.Common
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Removes special characters and gives simple string
+        /// </summary>
+        public static string SimplifyString(string stringWithSpecialChars)
+        {
+
+            // ReSharper disable once SuggestUseVarKeywordEvident
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in stringWithSpecialChars)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_' || c == '-')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
