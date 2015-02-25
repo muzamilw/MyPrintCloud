2e1450875bafd336ee7ed47b22b4e6738cd39edc
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-02-25 10:00:44 +0500

    smart form

--- Repository/Repositories/SmartFormRepository.cs
+++ Repository/Repositories/SmartFormRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Practices.Unity;
+using MPC.Common;
 using MPC.Interfaces.Repository;
 using MPC.Models.DomainModels;
 using MPC.Repository.BaseRepository;
@@ -42,6 +43,54 @@ namespace MPC.Repository.Repositories
         public TemplateVariable Find(int id)
         {
             return DbSet.Find(id);
+        }
+
+
+        public List<VariableList> GetVariablesData(bool isRealestateproduct,long storeId)
+        {
+            List<VariableList> objList = new List<VariableList>();
+                //if (isRealestateproduct)
+                //{
+                //    objList = from p in db.VariableSections
+                //              join es in db.FieldVariables on p.VariableSectionId equals es.VariableSectionId
+                //              where (es.VariableType == (int)TemplateVariableType.Global || es.VariableType == (int)TemplateVariableType.RealState || es.VariableType == (int)TemplateVariableType.RealStateImages)
+                //              orderby p.VariableSectionID, es.VariableType, es.SortOrder
+                //              select new
+                //              {
+                //                  SectionName = p.SectionName,
+                //                  VariableID = es.VariableID,
+                //                  VariableName = es.VariableName,
+                //                  VariableTag = es.VariableTag,
+                //                  VariableType = es.VariableType
+
+                //              };
+
+                //}
+                //else
+                //{
+                //    objList = from p in db.VariableSections
+                //              join es in db.FieldVariables on p.VariableSectionId equals es.VariableSectionId
+                //              where (es.VariableType == (int)TemplateVariableType.Global)
+                //              orderby es.VariableType, es.SortOrder
+                //              select new
+                //              {
+                //                  SectionName = p.SectionName,
+                //                  VariableID = es.VariableID,
+                //                  VariableName = es.VariableName,
+                //                  VariableTag = es.VariableTag,
+                //                  VariableType = es.VariableType
+
+                //              };
+                 
+                //}
+
+                return objList;
+        }
+        public enum TemplateVariableType
+        {
+            Global = 1,
+            RealState = 2,
+            RealStateImages = 3
         }
         #endregion
     }
