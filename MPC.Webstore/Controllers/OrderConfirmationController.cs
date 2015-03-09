0af0f1acb1353af3dc24eec069a8aeda692378a9
hadiabhoon <hadia@myprintcloud.com>
2015-03-09 11:28:20 +0500

    changes

--- MPC.Webstore/Controllers/OrderConfirmationController.cs
+++ MPC.Webstore/Controllers/OrderConfirmationController.cs
@@ -20,8 +20,9 @@ namespace MPC.Webstore.Controllers
         private readonly ICompanyService _myCompanyService;
         private readonly ICampaignService _myCampaignService;
         private readonly IUserManagerService _userManagerService;
+        private readonly ICampaignService _campaignService;
         public OrderConfirmationController(IOrderService OrderService, IWebstoreClaimsHelperService myClaimHelper, ICompanyService myCompanyService, IItemService ItemService
-            , ICampaignService myCampaignService, IUserManagerService userManagerService)
+            , ICampaignService myCampaignService, IUserManagerService userManagerService, ICampaignService campaignService)
         {
             if (OrderService == null)
             {
@@ -33,6 +34,7 @@ namespace MPC.Webstore.Controllers
             this._ItemService = ItemService;
             this._myCampaignService = myCampaignService;
             this._userManagerService = userManagerService;
+            this._campaignService = campaignService;
         }
         // GET: OrderConfirmation
         public ActionResult Index(string OrderId)
@@ -76,6 +78,7 @@ namespace MPC.Webstore.Controllers
                     }
 
                     ViewBag.Currency = StoreBaseResopnse.Currency;
+                    ViewBag.TaxLabel = StoreBaseResopnse.Company.TaxLabel;
                     StoreBaseResopnse = null;
                     return View("PartialViews/OrderConfirmation", shopCart);
                 }
@@ -128,13 +131,16 @@ namespace MPC.Webstore.Controllers
 
         private void PlaceOrder(int modOverride, long OrderId)
         {
+            string CacheKeyName = "CompanyBaseResponse";
+            ObjectCache cache = MemoryCache.Default;
+            
 
             bool result = false;
 
             PaymentGateway oPaymentGateWay = _ItemService.GetPaymentGatewayRecord(UserCookieManager.StoreId);
 
             CompanyContact user = _myCompanyService.GetContactByID(_myClaimHelper.loginContactID()); //LoginUser;
-           
+          
             CampaignEmailParams cep = new CampaignEmailParams();
             //    PageManager pageMgr = new PageManager();
             string HTMLOfShopReceipt = null;
@@ -147,13 +153,13 @@ namespace MPC.Webstore.Controllers
             Campaign OnlineOrderCampaign = _myCampaignService.GetCampaignRecordByEmailEvent((int)Events.OnlineOrder);
             if (user != null)
             {
-                MyCompanyDomainBaseResponse baseResponseOrganisation = _myCompanyService.GetStoreFromCache(UserCookieManager.StoreId).CreateFromOrganisation();
-
+                MPC.Models.ResponseModels.MyCompanyDomainBaseReponse baseResponse = (cache.Get(CacheKeyName) as Dictionary<long, MPC.Models.ResponseModels.MyCompanyDomainBaseReponse>)[UserCookieManager.StoreId];
+            
                 if (UserCookieManager.StoreMode == (int)StoreMode.Retail)
                 {
                     cep.StoreID = UserCookieManager.StoreId;
                     cep.AddressID = UserCookieManager.StoreId;
-                    if (true)//baseResponseOrganisation.Company.isEnableOnlinePayment == null || SessionParameters.CompanySite.isEnableOnlinePayment == false) // Utils.IsDemoMode() In demo mode set status to confirmed and don't go to paypal...
+                    if (user.IsPayByPersonalCreditCard == false || user.IsPayByPersonalCreditCard == null)//baseResponseOrganisation.Company.isEnableOnlinePayment == null || SessionParameters.CompanySite.isEnableOnlinePayment == false) // Utils.IsDemoMode() In demo mode set status to confirmed and don't go to paypal...
                     {
                         try
                         {
@@ -164,9 +170,10 @@ namespace MPC.Webstore.Controllers
                             string AttachmentPath = "";//emailmgr.OrderConfirmationPDF(OrderId, 0, 0);
                             List<string> AttachmentList = new List<string>();
                             AttachmentList.Add(AttachmentPath);
-                            SystemUser EmailOFSM = _userManagerService.GetSalesManagerDataByID(Convert.ToInt32(UserCookieManager.OrganisationID));
-                            //_myCampaignService.emailBodyGenerator(OnlineOrderCampaign, baseResponseOrganisation, cep, user, UserCookieManager.StoreMode, "", HTMLOfShopReceipt, "", EmailOFSM.Email, "", "", AttachmentList);
-                           // emailmgr.SendEmailToSalesManager((int)EmailEvents.NewOrderToSalesManager, SessionParameters.ContactID, SessionParameters.CustomerID, 0, OrderId, SessionParameters.CompanySite, 0, 0, StoreMode.Retail);
+                            SystemUser EmailOFSM = _userManagerService.GetSalesManagerDataByID(baseResponse.Company.SalesAndOrderManagerId1.Value);
+
+                            _myCampaignService.emailBodyGenerator(OnlineOrderCampaign, cep, user, (StoreMode)UserCookieManager.StoreMode, Convert.ToInt32(baseResponse.Organisation.OrganisationId), "", HTMLOfShopReceipt, "", EmailOFSM.Email, "", "", AttachmentList);
+                            _campaignService.SendEmailToSalesManager((int)Events.NewOrderToSalesManager, _myClaimHelper.loginContactID(), _myClaimHelper.loginContactCompanyID(), OrderId, UserCookieManager.OrganisationID, 0, StoreMode.Retail, UserCookieManager.StoreId, EmailOFSM);
                             UserCookieManager.OrderId = 0;
                             
                             // For demo mode as enter the pre payment with the known parameters
@@ -184,6 +191,7 @@ namespace MPC.Webstore.Controllers
                         }
                         catch (Exception ex)
                         {
+                            throw ex;
                             //MessgeToDisply.Visible = true;
                             //MessgeToDisply.Style.Add("border", "1px solid red");
                             //MessgeToDisply.Style.Add("font-size", "20px");
@@ -216,9 +224,7 @@ namespace MPC.Webstore.Controllers
                         }
                         else
                         {
-                            if (result)
-                            {
-
+                          
                                 switch (oPaymentGateWay.PaymentMethodId)
                                 {
                                     case 1: //PayPal
@@ -261,7 +267,7 @@ namespace MPC.Webstore.Controllers
                                     default:
                                         break;
                                 }
-                            }
+                           
                         }
                     }
                 }
@@ -270,7 +276,7 @@ namespace MPC.Webstore.Controllers
                     cep.StoreID = UserCookieManager.StoreId;
 
                     cep.AddressID = UserCookieManager.StoreId;
-                    SystemUser EmailOFSM = _userManagerService.GetSalesManagerDataByID(Convert.ToInt32(UserCookieManager.OrganisationID));
+                    SystemUser EmailOFSM = _userManagerService.GetSalesManagerDataByID(baseResponse.Company.SalesAndOrderManagerId1.Value);
                     cep.SystemUserID = EmailOFSM.SystemUserId;
                     if (((user.ContactRoleId == Convert.ToInt32(Roles.Adminstrator) || user.ContactRoleId == Convert.ToInt32(Roles.Manager)) && ((user.IsPayByPersonalCreditCard ?? false) == false)) || (modOverride == 3) || (user.ContactRoleId == Convert.ToInt32(Roles.User) && user.canUserPlaceOrderWithoutApproval == true && modOverride == 2) || (user.ContactRoleId == Convert.ToInt32(Roles.User) && user.canUserPlaceOrderWithoutApproval == true && user.IsPayByPersonalCreditCard == false)) // Corporate user that can approve the orders
                     {
