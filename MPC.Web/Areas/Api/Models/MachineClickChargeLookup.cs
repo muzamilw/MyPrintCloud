1692809c97a762fae8a7c628a787f4c53435d560
hadiabhoon <hadia@myprintcloud.com>
2015-03-10 14:59:59 +0500

    Lookup Methods

---
+++ MPC.Web/Areas/Api/Models/MachineClickChargeLookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MPC.MIS.Areas.Api.Models
+{
+    public class MachineClickChargeLookup
+    {
+        public long Id { get; set; }
+        public long MethodId { get; set; }
+        public double? SheetCost { get; set; }
+        public int? Sheets { get; set; }
+        public double? SheetPrice { get; set; }
+        public double? TimePerHour { get; set; }
+
+        public virtual LookupMethod LookupMethod { get; set; }
+    }
+}
