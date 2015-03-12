fdcdfad6e2f98cec24a608be37b4a8f18ecbd7e5
msarosh786 <msarosh@live.com>
2015-03-12 14:52:12 +0500

    Merging Changes

---
+++ MPC.Web/Areas/Settings/Scripts/MIS/sectionflags.dataservice.js
@@ -0,0 +1,43 @@
+﻿/*
+    Data service module with ajax calls to the server
+*/
+define("sectionflags/sectionflags.dataservice", function () {
+
+    // Data service for forecast 
+    var dataService = (function () {
+        var // True if initialized
+            isInitialized = false,
+            // Initialize
+            initialize = function () {
+                if (!isInitialized) {
+
+                    // Define request to get SectionFlags
+                    amplify.request.define('getSectionLibray', 'ajax', {
+                        url: ist.siteUrl + '/Api/SectionFlags',
+                        dataType: 'json',
+                        type: 'GET'
+                    });
+
+                    isInitialized = true;
+                }
+            },
+            // Get Sections by Section Id 
+              getSectionFlags = function (callbacks) {
+                  initialize();
+                  return amplify.request({
+                      resourceId:'getSectionLibray',
+                      success: callbacks.success,
+                      error: callbacks.error,
+                  });
+              };
+            // Save Prefixes
+           
+
+        return {
+            getSectionFlags: getSectionFlags
+            
+        };
+    })();
+
+    return dataService;
+});
---
+++ MPC.Web/Areas/Settings/Scripts/MIS/sectionflags.model.js
@@ -0,0 +1,97 @@
+﻿define(["ko", "underscore", "underscore-ko"], function (ko) {
+    var sectionflags = function () {
+
+        var
+            self,
+            sectionId = ko.observable(),
+            sectionName = ko.observable(),
+            secOrder = ko.observable(),
+            parentId = ko.observable(),
+            href = ko.observable(),
+            sectionImage = ko.observable(),
+            independent = ko.observable()
+            
+            
+
+        self =
+        {
+            sectionId:sectionId,
+            sectionName: sectionName,
+            secOrder: estimateStart,
+            parentId: estimateNext,
+            href: invoicePrefix,
+            sectionImage: invoiceStart,
+            independent: invoiceNext
+          
+        };
+        return self;
+    };
+
+    sectionFlagsListView = function (specifiedsectionId, specifiedsectionName, specifiedsecOrder, specifiedparentId, specifiedhref, specifiedsectionImage, specifiedindependent
+                           ) {
+        var
+            self,
+            //Unique ID
+            sectionId = ko.observable(specifiedsectionId),
+            //Name
+            sectionName = ko.observable(specifiedsectionName),
+            //Description
+            secOrder = ko.observable(specifiedsecOrder),
+            //Type
+            parentId = ko.observable(specifiedparentId),
+
+            href = ko.observable(specifiedhref),
+
+            sectionImage = ko.observable(specifiedsectionImage),
+
+            independent = ko.observable(specifiedindependent)
+
+           
+        self = {
+            sectionId: sectionId,
+            sectionName: sectionName,
+            secOrder: secOrder,
+            parentId: parentId,
+            href: href,
+            sectionImage: sectionImage,
+            independent: independent
+        };
+        return self;
+    };
+
+    sectionFlagsListView.Create = function (source) {
+        return new sectionFlagsListView(source.sectionId, source.sectionName, source.secOrder, source.parentId, source.href, source.sectionImage, source.independent);
+    };
+
+    var sectionFlagsClientMapper = function (source) {
+        var osectionflags = new sectionflags();
+        osectionflags.sectionId(source.SectionId);
+        osectionflags.sectionName(source.SectionName);
+        osectionflags.secOrder(source.SecOrder);
+        osectionflags.parentId(source.ParentId);
+        osectionflags.href(source.href);
+        osectionflags.sectionImage(source.SectionImage);
+        osectionflags.independent(source.Independent);
+        
+        return osectionflags;
+
+    };
+    var sectionFlagsServerMapper = function (source) {
+        var result = {};
+        result.SectionId = source.sectionId();
+        result.SectionName = source.sectionName();
+        result.SecOrder = source.secOrder();
+        result.ParentId = source.parentId();
+        result.href = source.href();
+        result.SectionImage = source.sectionImage();
+        result.Independent = source.independent();
+        
+        return result;
+    };
+
+    return {
+        prefix: sectionflags,
+        sectionFlagsClientMapper: sectionFlagsClientMapper,
+        sectionFlagsServerMapper: sectionFlagsServerMapper
+    };
+});
