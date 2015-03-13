475de6526d3519f16e963bbcc2a38b52956f8c65
iqramujahid <iqra@myprintcloud.com>
2015-03-13 20:30:34 +0500

    changes

---
+++ MPC.Webstore/Controllers/ProductParentCategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MPC.Interfaces.WebStoreServices;
+using MPC.Webstore.Common;
+using MPC.Models.Common;
+using MPC.Models.DomainModels;
+
+namespace MPC.Webstore.Controllers
+{
+    public class ProductParentCategoriesController : Controller
+    {
+          #region Private
+
+        private readonly ICompanyService _myCompanyService;
+        private readonly IWebstoreClaimsHelperService _myClaimHelper;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ProductParentCategoriesController(ICompanyService myCompanyService, IWebstoreClaimsHelperService myClaimHelper)
+        {
+            if (myCompanyService == null)
+            {
+                throw new ArgumentNullException("myCompanyService");
+            }
+            this._myCompanyService = myCompanyService;
+            this._myClaimHelper = myClaimHelper;
+        }
+
+        #endregion
+        // GET: ParentCategories
+        public ActionResult Index()
+        {
+            List<ProductCategory> lstParentCategories = new List<ProductCategory>();
+
+            if (UserCookieManager.StoreMode == (int)StoreMode.Corp)
+            {
+
+                Int64 roleid = _myClaimHelper.loginContactRoleID();
+
+                if (_myClaimHelper.loginContactID() != 0 && roleid == Convert.ToInt32(Roles.Adminstrator))
+                {
+                    lstParentCategories = _myCompanyService.GetAllParentCorporateCatalog((int)_myClaimHelper.loginContactCompanyID());
+                }
+                else
+                {
+                    lstParentCategories = _myCompanyService.GetAllParentCorporateCatalogByTerritory((int)_myClaimHelper.loginContactCompanyID(), (int)_myClaimHelper.loginContactID());
+                }
+
+
+            }
+            else
+            {
+                lstParentCategories = _myCompanyService.GetStoreParentCategories(UserCookieManager.StoreId, UserCookieManager.OrganisationID);
+            }
+
+            return PartialView("PartialViews/ProductParentCategories", lstParentCategories);
+        }
+    }
+}
