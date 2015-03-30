f1fbb5ae43a3a47377b928c2c3ff93a341a0af7b
Asad <asad@innostark.com>
2015-03-30 12:09:53 +0500

    Purchase work local commit

--- Interfaces/Repository/IPurchaseOrderRepository.cs
+++ Interfaces/Repository/IPurchaseOrderRepository.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MPC.Models.DomainModels;
+﻿using MPC.Models.DomainModels;
+using MPC.Models.RequestModels;
+using MPC.Models.ResponseModels;
 
 namespace MPC.Interfaces.Repository
 {
     public interface IPurchaseRepository: IBaseRepository<Purchase, long>
     {
+        PurchaseResponseModel GetPurchases(PurchaseRequestModel request);
     }
 }
