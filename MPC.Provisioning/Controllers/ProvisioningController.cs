a45655d4c954320ff954b3e00a0d2a771c6c12e6
zohaib-sikander <zohaibsikander@gmail.com>
2015-04-01 17:19:02 +0500

    change

--- MPC.Provisioning/Controllers/ProvisioningController.cs
+++ MPC.Provisioning/Controllers/ProvisioningController.cs
@@ -171,7 +171,7 @@ namespace MPC.Provisioning.Controllers
                         //var iTask = request.GetRequestStreamAsync();
                         //Task.WaitAll(iTask);
                         request.Method = "GET";
-                        request.Timeout = 140000;
+                        request.Timeout = 180000;
                         using (WebResponse response = request.GetResponse())
                         {
                             using (Stream stream = response.GetResponseStream())
