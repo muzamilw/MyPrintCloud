16d79804d7d9a690c345ed711140987a9d804801
zohaib-sikander <zohaibsikander@gmail.com>
2015-04-01 14:30:23 +0500

    changes

--- Repository/Repositories/OrganisationRepository.cs
+++ Repository/Repositories/OrganisationRepository.cs
@@ -3300,10 +3300,18 @@ namespace MPC.Repository.Repositories
         }
         void Copy(string sourceDir, string targetDir)
         {
-            Directory.CreateDirectory(targetDir);
+            if(!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+          
 
             foreach (var file in Directory.GetFiles(sourceDir))
-                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+            {
+                
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)),true);
+            }
+                
 
             foreach (var directory in Directory.GetDirectories(sourceDir))
                 Copy(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
