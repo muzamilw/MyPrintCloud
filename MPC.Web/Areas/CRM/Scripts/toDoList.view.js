795c34be14f592c2030c6d892d8d9390f7387ae5
rafiq-innostark <rafiq@innostark.com>
2015-04-09 18:10:25 +0500

    CRM > To Do List

---
+++ MPC.Web/Areas/CRM/Scripts/toDoList.view.js
@@ -0,0 +1,57 @@
+﻿/*
+    View for the To Do List. Used to keep the viewmodel clear of UI related logic
+*/
+define("toDoList/toDoList.view",
+    ["jquery", "toDoList/toDoList.viewModel"], function ($, toDoListViewModel) {
+
+        var ist = window.ist || {};
+
+        // View 
+        ist.toDoList.view = (function (specifiedViewModel) {
+            var
+                // View model 
+                viewModel = specifiedViewModel,
+                // Binding root used with knockout
+                bindingRoot = $("#toDoListBinding")[0],
+                //// Show Calendar Activity dialog
+                showCalendarActivityDialog = function () {
+                    $("#calendarActivityModal").modal("show");
+                },
+                // Hide Calendar Activity dialog
+                hideCalendarActivityDialog = function () {
+                    $("#calendarActivityModal").modal("hide");
+                },
+
+                  // Show Contact dialog
+                showContactSelectorDialog = function () {
+                    $("#contactSelectorDialog").modal("show");
+                },
+                // Hide Contact dialog
+                hideContactSelectorDialog = function () {
+                    $("#contactSelectorDialog").modal("hide");
+                },
+
+                // Initialize
+                initialize = function () {
+                    if (!bindingRoot) {
+                        return;
+                    }
+                };
+            initialize();
+            return {
+                bindingRoot: bindingRoot,
+                viewModel: viewModel,
+                showCalendarActivityDialog: showCalendarActivityDialog,
+                hideCalendarActivityDialog: hideCalendarActivityDialog,
+                showContactSelectorDialog: showContactSelectorDialog,
+                hideContactSelectorDialog: hideContactSelectorDialog
+
+            };
+        })(toDoListViewModel);
+
+        // Initialize the view model
+        if (ist.toDoList.view.bindingRoot) {
+            toDoListViewModel.initialize(ist.toDoList.view);
+        }
+        return ist.toDoList.view;
+    });
