3f3a5807f8060d32e894e331413600c2027cf415
Muhammad Naveed Zahid <mnaveedzahid@gmail.com>
2015-04-10 08:27:54 +0500

    Cost Center Execution

---
+++ Models/RequestModels/CostCenterExecutionRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPC.Models.Common;
+
+namespace MPC.Models.RequestModels
+{
+    public class CostCenterExecutionRequest
+    {
+        public long CostCenterId { get; set; }
+        public long ItemId { get; set; }
+        public int Quantity { get; set; }
+        public string Action { get; set; }
+        public List<QuestionQueueItem> QuestionQueueItems { get; set; }
+    }
+}
