1d1399d3b1743b83bf16a6971ee22d66fa022992
Asad <asad@innostark.com>
2015-04-13 15:36:38 +0500

    Dashboard plus pagination work set

--- Models/ResponseModels/OrderStatusesResponse.cs
+++ Models/ResponseModels/OrderStatusesResponse.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Collections.Generic;
+using MPC.Models.DomainModels;
+
 namespace MPC.MIS.Areas.Api.Models
 {
     /// <summary>
@@ -40,6 +43,11 @@ namespace MPC.MIS.Areas.Api.Models
         /// Estimate Total
         /// </summary>
         public double? TotalEarnings { get; set; }
+
+        /// <summary>
+        /// Estimates / Orders list
+        /// </summary>
+        public IEnumerable<Estimate> Estimates{ get; set; }
 
         
     }
