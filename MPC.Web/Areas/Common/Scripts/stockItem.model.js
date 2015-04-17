adb0fe6228ce5f688f60df258bc71c6ad63d75f7
khurram <khurram@innostark.com>
2015-04-17 12:41:13 +0500

    Stock Item > Selection not working

--- MPC.Web/Areas/Common/Scripts/stockItem.model.js
+++ MPC.Web/Areas/Common/Scripts/stockItem.model.js
@@ -2,68 +2,16 @@
    // #region __________________  I N V E N T O R Y   ______________________
 
     // ReSharper disable once InconsistentNaming
-    var StockItem = function (specifiedId, specifiedname,
-        specifiedWeight, specifiedPackageQty, specifiedPerQtyQty, specifiedPrice) {
+    var StockItem = function (specifiedId, specifiedname, specifiedWeight, specifiedPackageQty, specifiedPerQtyQty, specifiedPrice) {
 
-        var self,
-            stockItemId = ko.observable(specifiedId),
-            itemName = ko.observable(specifiedname),
-            itemWeight = ko.observable(specifiedWeight),
-            packageQty = ko.observable(specifiedPackageQty),
-            perQtyQty = ko.observable(specifiedPerQtyQty),
-            price = ko.observable(specifiedPrice),
-            errors = ko.validation.group({
-
-            }),
-            // Is Valid 
-            isValid = ko.computed(function () {
-                return errors().length === 0 ? true : false;
-            }),
-
-
-            // ReSharper disable InconsistentNaming
-            dirtyFlag = new ko.dirtyFlag({
-                stockItemId: stockItemId,
-                itemName: itemName,
-                itemWeight: itemWeight,
-                packageQty: packageQty,
-                perQtyQty: perQtyQty,
-                price: price
-            }),
-            // Has Changes
-            hasChanges = ko.computed(function () {
-                return dirtyFlag.isDirty();
-            }),
-            //Convert To Server
-            convertToServerData = function () {
-                return {
-                    StockItemId: stockItemId(),
-                    ItemName: itemName(),
-                    ItemWeight: itemWeight(),
-                    PackageQty: packageQty(),
-                    PerQtyQty: perQtyQty(),
-                    Price: price()
-                };
-            },
-            // Reset
-            reset = function () {
-                dirtyFlag.reset();
-            };
-        self = {
-            stockItemId: stockItemId,
-            itemName: itemName,
-            itemWeight: itemWeight,
-            packageQty: packageQty,
-            perQtyQty: perQtyQty,
-            price: price,
-            isValid: isValid,
-            errors: errors,
-            dirtyFlag: dirtyFlag,
-            hasChanges: hasChanges,
-            convertToServerData: convertToServerData,
-            reset: reset
+        return {
+            id: specifiedId,
+            name: specifiedname,
+            itemWeight: specifiedWeight,
+            packageQty: specifiedPackageQty,
+            perQtyQty: specifiedPerQtyQty,
+            price: specifiedPrice
         };
-        return self;
     };
 
     StockItem.Create = function (source) {
