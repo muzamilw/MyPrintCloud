13d2e4862414cecc603cf3d3f0227e65f9de3b14
iqramujahid <iqra@myprintcloud.com>
2015-04-22 15:27:22 +0500

    changes

--- MPC.Webstore/Scripts/PopUp.js
+++ MPC.Webstore/Scripts/PopUp.js
@@ -7,15 +7,23 @@
     document.getElementById("layer").style.width = bws.width + "px";
     document.getElementById("layer").style.height = bws.height + "px";
     
-    var left = parseInt((bws.width - 500) / 2);
+    var left = 0;
     var top = parseInt((bws.height - 170) / 2);
+
+    if (bws.width <  640) {
+        document.getElementById("innerLayer").style.width = (bws.width) + "px";
+        
+    } else {
+        left = parseInt((bws.width - 500) / 2);
+        document.getElementById("innerLayer").style.width = "500px";
+    }
 
     document.getElementById("innerLayer").innerHTML = container;
 
     document.getElementById("innerLayer").style.top = top + "px";
     document.getElementById("innerLayer").style.left = left + "px";
 
-    document.getElementById("innerLayer").style.width = "500px";
+   
     document.getElementById("innerLayer").style.height = "170px";
     document.getElementById("innerLayer").style.position = "fixed";
     document.getElementById("innerLayer").style.zIndex = "9999";
@@ -208,12 +216,24 @@ function ShowArtWorkPopup(Type, panelHtml) {
         container = '<div class="md-modal md-effect-7 col-xs-12" id="modal-7" ><div class="md-content"><div class="modal-header"><button class="md-close close" onclick=HideMessagePopUp(); >&times;</button><h4 class="modal-title">' + Type + '</h4></div><div class="modal-body" style="overflow-y:scroll;height:350px;">' + panelHtml + '</div></div>';
 
     }
-    else if (bws.width < 640) {
+    else if (bws.width < 640 && bws.width > 600) {
         //left = parseInt((bws.width) / 2);
         document.getElementById("innerLayer").style.width = (bws.width) + "px";
         container = '<div class="md-modal md-effect-7 col-xs-12" id="modal-7" ><div class="md-content"><div class="modal-header"><button class="md-close close" onclick=HideMessagePopUp(); >&times;</button><h4 class="modal-title">' + Type + '</h4></div><div class="modal-body" style="overflow-y:scroll;height:550px;">' + panelHtml + '</div></div>';
 
-    } else
+    }
+    else if (bws.width < 600 && bws.width > 500) {
+        //left = parseInt((bws.width) / 2);
+        document.getElementById("innerLayer").style.width = (bws.width) + "px";
+        container = '<div class="md-modal md-effect-7 col-xs-12" id="modal-7" ><div class="md-content"><div class="modal-header"><button class="md-close close" onclick=HideMessagePopUp(); >&times;</button><h4 class="modal-title">' + Type + '</h4></div><div class="modal-body" style="overflow-y:scroll;height:350px;">' + panelHtml + '</div></div>';
+
+    } else if (bws.width < 500) {
+        //left = parseInt((bws.width) / 2);
+        document.getElementById("innerLayer").style.width = (bws.width) + "px";
+        container = '<div class="md-modal md-effect-7 col-xs-12" id="modal-7" ><div class="md-content"><div class="modal-header"><button class="md-close close" onclick=HideMessagePopUp(); >&times;</button><h4 class="modal-title">' + Type + '</h4></div><div class="modal-body" style="overflow-y:scroll;height:550px;">' + panelHtml + '</div></div>';
+
+    }
+    else
     {
 
         left = parseInt((bws.width - 730) / 2);
