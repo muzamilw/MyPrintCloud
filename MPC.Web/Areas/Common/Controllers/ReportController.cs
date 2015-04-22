1831db557982b2e794b07cc5672712c90122399c
hadiabhoon <hadia@myprintcloud.com>
2015-04-22 11:16:14 +0500

    changes

---
+++ MPC.Web/Areas/Common/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPC.MIS.Areas.Common.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Common/Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
