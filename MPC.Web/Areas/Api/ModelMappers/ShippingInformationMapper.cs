8972c836866f1c32bde175321a2068517d2a4838
khurram <khurram@innostark.com>
2015-04-24 14:25:07 +0500

    order > Shipping Information

---
+++ MPC.Web/Areas/Api/ModelMappers/ShippingInformationMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace MPC.MIS.Areas.Api.ModelMappers
+{
+    /// <summary>
+    /// Shipping Information Mapper
+    /// </summary>
+    public static class ShippingInformationMapper
+    {
+        #region Public
+        
+        #endregion
+    }
+}
