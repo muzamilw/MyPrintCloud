088b24292a0d981a2effe83857509ea46fab6dbe
khurram <khurram@innostark.com>
2015-05-28 19:04:40 +0500

    changes

--- MPC.Web/Areas/Api/Models/Machine.cs
+++ MPC.Web/Areas/Api/Models/Machine.cs
@@ -80,8 +80,6 @@ namespace MPC.MIS.Areas.Api.Models
         public bool? isSheetFed { get; set; }
         public int? Passes { get; set; }
         public bool? IsSpotColor { get; set; }
-
-        public bool? IsSpotColor { get; set; }
         public IEnumerable<MachineInkCoverage> MachineInkCoverages { get; set; }
     }
 }
