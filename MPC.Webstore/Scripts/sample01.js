aee3feffb558859c2c3589c6e9f6885b8eb9adfb
awais ghani <awais@myprintcloud.com>
2015-06-04 11:35:35 +0500

    changes

--- MPC.Webstore/Scripts/sample01.js
+++ MPC.Webstore/Scripts/sample01.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
-    alert();
-    $('#realEstateCaouselr').carousel({ carouselWidth: 700, carouselHeight: 195, directionNav: true, shadow: true, buttonNav: 'bullets' });
+﻿
+$(document).ready(function () {
+   
+    $('.carousel').carousel({ carouselWidth: 700, carouselHeight: 195, directionNav: true, shadow: true, buttonNav: 'bullets' });
 });
