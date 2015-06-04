588737038e4349a7d288909b91b37221300878ee
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-06-04 11:34:18 +0500

    slider realestate

--- MPC.Webstore/Scripts/sample01.js
+++ MPC.Webstore/Scripts/sample01.js
@@ -1,4 +1,4 @@
 ﻿$(document).ready(function () {
-    alert();
+
     $('#realEstateCaouselr').carousel({ carouselWidth: 700, carouselHeight: 195, directionNav: true, shadow: true, buttonNav: 'bullets' });
 });
