2369f42a1c459837f0a744b8d752e4db0b190e4b
rafiq-innostark <rafiq@innostark.com>
2015-06-05 19:02:49 +0500

    Purchase Order

--- Interfaces/Repository/IPurchaseRepository.cs
+++ Interfaces/Repository/IPurchaseRepository.cs
@@ -4,8 +4,13 @@ using MPC.Models.ResponseModels;
 
 namespace MPC.Interfaces.Repository
 {
-    public interface IPurchaseRepository: IBaseRepository<Purchase, long>
+    public interface IPurchaseRepository : IBaseRepository<Purchase, long>
     {
         PurchaseResponseModel GetPurchases(PurchaseRequestModel request);
+
+        /// <summary>
+        /// Get Purchase Orders
+        /// </summary>
+        PurchaseResponseModel GetPurchaseOrders(PurchaseOrderSearchRequestModel request);
     }
 }
