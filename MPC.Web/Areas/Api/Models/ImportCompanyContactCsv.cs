7575113f056f2fe4cb5bd2914a81ddfd3ce26859
Asad <asad@innostark.com>
2015-06-08 19:10:48 +0500

    Importing Company Contacts

---
+++ MPC.Web/Areas/Api/Models/ImportCompanyContactCsv.cs
@@ -0,0 +1,9 @@
+﻿namespace MPC.MIS.Areas.Api.Models
+{
+    public class ImportCompanyContactCsv
+    {
+        public string FileName { get; set; }
+        public string FileBytes { get; set; }
+        public long CompanyId { get; set; }
+    }
+}
