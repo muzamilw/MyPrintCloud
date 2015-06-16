f9e96070fe5fc06d254cb882100ea53a661445ce
khurram <khurram@innostark.com>
2015-06-16 14:45:26 +0500

    Product > Set Defaults to New Product

---
+++ MPC.Web/Areas/Api/Models/StockItemDropDownForProduct.cs
@@ -0,0 +1,44 @@
+﻿namespace MPC.MIS.Areas.Api.Models
+{
+    /// <summary>
+    /// Stock Item used in Product for default selection
+    /// </summary>
+    public class StockItemDropDownForProduct
+    {
+        /// <summary>
+        /// Stock Item ID
+        /// </summary>
+        public long StockItemId { get; set; }
+
+        /// <summary>
+        /// Item Code
+        /// </summary>
+        public string ItemCode { get; set; }
+
+        /// <summary>
+        /// Item Name
+        /// </summary>
+        public string ItemName { get; set; }
+
+        /// <summary>
+        /// Alternate Name
+        /// </summary>
+        public string AlternateName { get; set; }
+
+        /// <summary>
+        /// Package Quantity
+        /// </summary>
+        public double? PackageQty { get; set; }
+
+        /// <summary>
+        /// In Stock
+        /// </summary>
+        public double? InStock { get; set; }
+
+        /// <summary>
+        /// Allocated
+        /// </summary>
+        public double? Allocated { get; set; }
+
+    }
+}
