8e7f7261063e3db84bf4f02e522db5f3fa811ae2
asad-innostark <asad@innostark.com>
2015-06-19 09:33:35 +0500

    company contacts for order api

---
+++ MPC.Web/Areas/Api/Controllers/CompanyContactForOrderController.cs
@@ -0,0 +1,40 @@
+﻿using MPC.Interfaces.MISServices;
+using MPC.MIS.Areas.Api.ModelMappers;
+using MPC.MIS.Areas.Api.Models;
+using MPC.Models.RequestModels;
+using System.Web.Http;
+
+namespace MPC.MIS.Areas.Api.Controllers
+{
+    /// <summary>
+    /// Company Contact For Order API Controller
+    /// </summary>
+    public class CompanyContactForOrderController : ApiController
+    {
+        #region Private
+
+        private readonly ICompanyContactService _companyContactService;
+
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CompanyContactForOrderController(ICompanyContactService companyContactService)
+        {
+            _companyContactService = companyContactService;
+        }
+
+        #endregion
+        #region Public
+        /// <summary>
+        /// Get Contacts for Order 
+        /// </summary>
+        public ContactResponseForOrder Get([FromUri]CompanyRequestModelForCalendar request)
+        {
+           return _companyContactService.GetContactsForOrder(request).CreateFrom();
+        }
+        #endregion
+    }
+}
