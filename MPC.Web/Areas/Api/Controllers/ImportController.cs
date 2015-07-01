1112179374876c33a3fb74aa26ec3e3d4e29437c
Asad <asad@innostark.com>
2015-07-01 15:26:31 +0500

    Importing Company Contact by pipeline delimiter

--- MPC.Web/Areas/Api/Controllers/ImportController.cs
+++ MPC.Web/Areas/Api/Controllers/ImportController.cs
@@ -32,50 +32,51 @@ namespace MPC.MIS.Areas.Api.Controllers
         }
         #endregion
         #region Public
-        
 
-         [ApiException]
-         [ApiAuthorize(AccessRights = new[] { SecurityAccessRight.CanViewStore })]
-         [CompressFilter]
+
+        [ApiException]
+        [ApiAuthorize(AccessRights = new[] { SecurityAccessRight.CanViewStore })]
+        [CompressFilter]
         public void Post(ImportCompanyContactCsv request)
-         {
-             string base64 = request.FileBytes.Substring(request.FileBytes.IndexOf(',') + 1);
-             base64 = base64.Trim('\0');
-             byte[] data = Convert.FromBase64String(base64);
-             string directoryPath = HttpContext.Current.Server.MapPath("~/MPC_Content/Assets/SampleCsv");
-             if (directoryPath != null && !Directory.Exists(directoryPath)){
-                 Directory.CreateDirectory(directoryPath);
-             }
-             string savePath = directoryPath + "\\" + "_companyContact.csv";
-             File.WriteAllBytes(savePath, data);
-             int indexOf = savePath.LastIndexOf("MPC_Content", StringComparison.Ordinal);
-             savePath = savePath.Substring(indexOf, savePath.Length - indexOf);
+        {
+            string base64 = request.FileBytes.Substring(request.FileBytes.IndexOf(',') + 1);
+            base64 = base64.Trim('\0');
+            byte[] data = Convert.FromBase64String(base64);
+            string directoryPath = HttpContext.Current.Server.MapPath("~/MPC_Content/Assets/SampleCsv");
+            if (directoryPath != null && !Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            string savePath = directoryPath + "\\" + "_companyContact.csv";
+            File.WriteAllBytes(savePath, data);
+            int indexOf = savePath.LastIndexOf("MPC_Content", StringComparison.Ordinal);
+            savePath = savePath.Substring(indexOf, savePath.Length - indexOf);
 
-             string mapPath = HostingEnvironment.MapPath("~/"+savePath);
-             if (mapPath != null)
-             {
+            string mapPath = HostingEnvironment.MapPath("~/" + savePath);
+            if (mapPath != null)
+            {
                 // ReSharper disable once PossiblyMistakenUseOfParamsMethod
-                 var filePath = Path.Combine(mapPath);
-                 FileHelperEngine<ImportCompanyContact> engine = new FileHelperEngine<ImportCompanyContact>();
+                var filePath = Path.Combine(mapPath);
+                FileHelperEngine<ImportCompanyContact> engine = new FileHelperEngine<ImportCompanyContact>();
 
-                 ImportCompanyContact[] dataLoaded = engine.ReadFile(filePath);
-                 List<ImportCompanyContact> tmp = new List<ImportCompanyContact>(dataLoaded);
-                 tmp.RemoveAt(0);
-                 dataLoaded = tmp.ToArray();
-                 if (dataLoaded.Any())
-                 {
-                     IEnumerable<StagingImportCompanyContactAddress> enumerable = dataLoaded.Select(x => x.Createfrom(request.CompanyId)).ToList();
-                     //List<StagingImportCompanyContactAddress> list = enumerable as List<StagingImportCompanyContactAddress>;
-                     //list.Remove(list[0]);
-                     //enumerable = list;
-                     companyService.SaveImportedCompanyContact(enumerable);
-                 }
-             }
-             if (File.Exists(savePath))
-             {
-                 File.Delete(savePath);
-             }
-         }
+                ImportCompanyContact[] dataLoaded = engine.ReadFile(filePath);
+                //List<ImportCompanyContact> tmp = new List<ImportCompanyContact>(dataLoaded);
+                //tmp.RemoveAt(0);
+                //dataLoaded = tmp.ToArray();
+                if (dataLoaded.Any())
+                {
+                    IEnumerable<StagingImportCompanyContactAddress> enumerable = dataLoaded.Select(x => x.Createfrom(request.CompanyId)).ToList();
+                    //List<StagingImportCompanyContactAddress> list = enumerable as List<StagingImportCompanyContactAddress>;
+                    //list.Remove(list[0]);
+                    //enumerable = list;
+                    companyService.SaveImportedCompanyContact(enumerable);
+                }
+                if (File.Exists(savePath))
+                {
+                    File.Delete(savePath);
+                }
+            }
+        }
         #endregion
     }
 }
