37a6a96a39dd4774a831e47063e68643e6caa066
asad-innostark <asad@innostark.com>
2015-07-14 15:13:10 +0500

    commit

---
+++ Interfaces/MISServices/IDiscountVoucherService .cs
@@ -0,0 +1,13 @@
+﻿using MPC.Models.RequestModels;
+using MPC.Models.ResponseModels;
+
+namespace MPC.Interfaces.MISServices
+{
+    public interface IDiscountVoucherService 
+    {
+        /// <summary>
+        /// Discount Voucher List view 
+        /// </summary>
+        DiscountVoucherListViewResponse GetDiscountVoucherListView(DiscountVoucherRequestModel requestModel); 
+    }
+}
---
+++ Models/RequestModels/DiscountVoucherRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MPC.Models.RequestModels
+{
+    /// <summary>
+    /// Activity Request Model
+    /// </summary>
+    public class DiscountVoucherRequestModel : GetPagedListRequest
+    {
+        public string SearchString { get; set; }
+    }
+}
