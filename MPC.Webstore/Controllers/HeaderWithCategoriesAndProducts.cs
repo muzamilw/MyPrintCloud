a000c136aabaa260d52f7ac49d4deb6e09c7464d
awais ghani <awais@myprintcloud.com>
2015-07-28 16:53:48 +0500

    changes

--- MPC.Webstore/Controllers/HeaderWithCategoriesAndProducts.cs
+++ MPC.Webstore/Controllers/HeaderWithCategoriesAndProducts.cs
@@ -4,6 +4,7 @@ using MPC.Webstore.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +15,7 @@ namespace MPC.Webstore.Controllers
         // GET: FutureFormHeader
         private ICompanyService _myCompanyService;
 
-        public ProductCategoriesHeaderController(ICompanyService _myCompanyService)
+        public HeaderWithCategoriesAndProductsController(ICompanyService _myCompanyService)
         {
             this._myCompanyService = _myCompanyService;
         }
@@ -23,10 +24,16 @@ namespace MPC.Webstore.Controllers
             List<ProductCategory> AllCategroies = new List<ProductCategory>();
             //List<ProductCategory> ChildCategories = new List<ProductCategory>();
             AllCategroies = _myCompanyService.GetAllCategories(UserCookieManager.WBStoreId, UserCookieManager.WEBOrganisationID);
-            ViewBag.ParentCategory = AllCategroies.Where(i => i.ParentCategoryId == null || i.ParentCategoryId == 0).OrderBy(g => g.DisplayOrder).ToList();
+            ViewBag.ParentCategory = AllCategroies.Where(i => i.ParentCategoryId == null || i.ParentCategoryId == 0).OrderBy(g => g.DisplayOrder).Take(4).ToList();
             // ChildCategories=_myCompanyService.GetAllCategories(UserCookieManager.WBStoreId);
             ViewBag.AllChildCategories = AllCategroies.Where(i => i.ParentCategoryId != null || i.ParentCategoryId != 0).OrderBy(g => g.DisplayOrder).ToList();
-            return PartialView("PartialViews/HeaderWithCategoriesAndProducts");
+
+            MPC.Models.DomainModels.Company model = null;
+            string CacheKeyName = "CompanyBaseResponse";
+            ObjectCache cache = MemoryCache.Default;
+            //   MyCompanyDomainBaseResponse baseResponse = _myCompanyService.GetStoreFromCache(UserCookieManager.StoreId).CreateFromCompany();
+            MPC.Models.ResponseModels.MyCompanyDomainBaseReponse StoreBaseResopnse = (cache.Get(CacheKeyName) as Dictionary<long, MPC.Models.ResponseModels.MyCompanyDomainBaseReponse>)[UserCookieManager.WBStoreId];
+            return PartialView("PartialViews/HeaderWithCategoriesAndProducts", StoreBaseResopnse.Company);
         }
     }
 }
