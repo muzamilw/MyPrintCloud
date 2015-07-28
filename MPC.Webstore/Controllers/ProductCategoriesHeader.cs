afa55aa75748d26f8c957657a9e43f5ae62ae238
awais ghani <awais@myprintcloud.com>
2015-07-28 11:28:23 +0500

    changings

---
+++ MPC.Webstore/Controllers/ProductCategoriesHeader.cs
@@ -0,0 +1,32 @@
+﻿using MPC.Interfaces.WebStoreServices;
+using MPC.Models.DomainModels;
+using MPC.Webstore.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPC.Webstore.Controllers
+{
+    public class ProductCategoriesHeaderController : Controller
+    {
+        // GET: FutureFormHeader
+        private ICompanyService _myCompanyService;
+
+        public ProductCategoriesHeaderController(ICompanyService _myCompanyService)
+        {
+            this._myCompanyService = _myCompanyService;
+        }
+        public ActionResult Index()
+        {
+            List<ProductCategory> AllCategroies = new List<ProductCategory>();
+            //List<ProductCategory> ChildCategories = new List<ProductCategory>();
+            AllCategroies = _myCompanyService.GetAllCategories(UserCookieManager.WBStoreId, UserCookieManager.WEBOrganisationID);
+            ViewBag.ParentCategory = AllCategroies.Where(i => i.ParentCategoryId == null || i.ParentCategoryId == 0).OrderBy(g => g.DisplayOrder).ToList();
+            // ChildCategories=_myCompanyService.GetAllCategories(UserCookieManager.WBStoreId);
+            ViewBag.AllChildCategories = AllCategroies.Where(i => i.ParentCategoryId != null || i.ParentCategoryId != 0).OrderBy(g => g.DisplayOrder).ToList();
+            return PartialView("PartialViews/ProductCategoriesHeader");
+        }
+    }
+}
