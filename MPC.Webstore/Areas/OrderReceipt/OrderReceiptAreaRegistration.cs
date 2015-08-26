e3a5172acd7e9e19c79916c8369062fbd7d698eb
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-08-26 15:44:11 +0500

    receipt page

---
+++ MPC.Webstore/Areas/OrderReceipt/OrderReceiptAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MPC.Webstore.Areas.OrderReceipt
+{
+    public class OrderReceiptAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "OrderReceipt";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                     "ReceiptDefault_MultiParams",
+                    AreaName + "/{OrderId}/{StoreId}/{IsPrintReceipt}",
+                     new { controller = "OrderReceipt", action = "Index", OrderId = RouteParameter.Optional, StoreId = RouteParameter.Optional, IsPrintReceipt = RouteParameter.Optional }
+               );
+        }
+    }
+}
