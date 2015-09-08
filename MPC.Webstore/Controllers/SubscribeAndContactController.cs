d557dfd7f075ce54f05030a1ed7242664ccb1618
awais ghani <awais@myprintcloud.com>
2015-09-08 14:11:59 +0500

    chnges

--- MPC.Webstore/Controllers/SubscribeAndContactController.cs
+++ MPC.Webstore/Controllers/SubscribeAndContactController.cs
@@ -49,7 +49,7 @@ namespace MPC.Webstore.Controllers
             //ObjectCache cache = MemoryCache.Default;
             //MPC.Models.ResponseModels.MyCompanyDomainBaseReponse StoreBaseResopnse = (cache.Get(CacheKeyName) as Dictionary<long, MPC.Models.ResponseModels.MyCompanyDomainBaseReponse>)[UserCookieManager.WBStoreId];
             MyCompanyDomainBaseReponse StoreBaseResopnse = _myCompanyService.GetStoreCachedObject(UserCookieManager.WBStoreId);
-
+            ViewBag.Company = StoreBaseResopnse.Company;
             if (StoreBaseResopnse.StoreDetaultAddress != null)
             {
                 oAddress = new AddressViewModel();
@@ -85,7 +85,8 @@ Utils.GetKeyValueFromResourceFile("ltrllemail", UserCookieManager.WBStoreId, "Em
             try
             {
 
-
+                MyCompanyDomainBaseReponse StoreBaseResopnse = _myCompanyService.GetStoreCachedObject(UserCookieManager.WBStoreId);
+                ViewBag.Company = StoreBaseResopnse.Company;
                 NewsLetterSubscriber subscriber = _myCompanyService.GetSubscriber(txtEmailbox, UserCookieManager.WBStoreId);
 
 
