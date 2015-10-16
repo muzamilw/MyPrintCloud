32cb08755571a490930cda0ae075fa0a08b27ab5
iqramujahid <iqra@myprintcloud.com>
2015-10-16 17:14:26 +0500

    changes

--- Repository/Repositories/ScopeVariableRepository.cs
+++ Repository/Repositories/ScopeVariableRepository.cs
@@ -46,7 +46,30 @@ namespace MPC.Repository.Repositories
 
         }
 
+        public void AddScopeVariables(long ContactId, long CompanyId) 
+        {
+            // add scope variables
 
+            List<FieldVariable> listOfCompanyVariables = db.FieldVariables.Where(f => f.CompanyId == CompanyId && f.Scope == 2).ToList();
+            List<ScopeVariable> listOfUserScopeVariables = new List<ScopeVariable>();
+            ScopeVariable UserScopeVariable = null;
+            foreach (FieldVariable vari in listOfCompanyVariables)
+            {
+                UserScopeVariable = new ScopeVariable();
+                UserScopeVariable.VariableId = vari.VariableId;
+                UserScopeVariable.Id = ContactId;
+                UserScopeVariable.Value = vari.DefaultValue;
+                UserScopeVariable.Scope = 2;
+                listOfUserScopeVariables.Add(UserScopeVariable);
+            }
+
+            if (listOfUserScopeVariables.Count() > 0)
+            {
+                db.ScopeVariables.AddRange(listOfUserScopeVariables);
+            }
+
+            db.SaveChanges();
+        }
         #endregion
     }
 }
