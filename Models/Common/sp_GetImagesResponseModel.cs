386f03b4de7a46a455fb2872a7f90c310fd73bbc
hadiabhoon <hadia@myprintcloud.com>
2015-10-26 18:09:20 +0500

    designer changes

---
+++ Models/Common/sp_GetImagesResponseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Models.Common
+{
+    public class sp_GetImagesResponseModel
+    {
+        public long? RowNum { get; set; }
+        public int? ID { get; set; }
+        public int? ProductID { get; set; }
+        public string ImageName { get; set; }
+        public string Name { get; set; }
+        public string BackgroundImageRelativePath { get; set; }
+        public int? ImageType { get; set; }
+        public int? ImageWidth { get; set; }
+        public int? ImageHeight { get; set; }
+        public string ImageTitle { get; set; }
+        public string ImageDescription { get; set; }
+        public string ImageKeywords { get; set; }
+    }
+}
