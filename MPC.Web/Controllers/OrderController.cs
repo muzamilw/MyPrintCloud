434de9dd16dd4a5863939e255758bd54e0f39cc8
Muhammad Naveed Zahid <mnaveedzahid@gmail.com>
2015-10-29 19:09:40 +0500

    Changes for Purchase Order link in Email Service separated for Web Store

--- MPC.Web/Controllers/OrderController.cs
+++ MPC.Web/Controllers/OrderController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using MPC.Interfaces.MISServices;
 
 namespace MPC.MIS.Controllers
 {
     public class OrderController : Controller
     {
+        
         public ActionResult OrderList()
         {
             return View();
@@ -25,7 +23,7 @@ namespace MPC.MIS.Controllers
             return View();
         }
 
-       
+        
 
     }
 }
