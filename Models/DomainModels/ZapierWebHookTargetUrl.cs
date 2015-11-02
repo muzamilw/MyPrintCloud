45d1a8e788ac9b11c5e5cd3e88d8cdae9317f728
Muhammad Naveed Zahid <mnaveedzahid@gmail.com>
2015-11-02 11:12:51 +0500

    Table added for zapier webhook url

---
+++ Models/DomainModels/ZapierWebHookTargetUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Models.DomainModels
+{
+    public class ZapierWebHookTargetUrl
+    {
+        public long ZapierTargetUrlId { get; set; }
+        public string TargetUrl { get; set; }
+        public int? WebHookEvent { get; set; }
+    }
+}
