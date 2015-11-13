4fb5ba01988ef7cefcbb78fbf7c5687fb2680468
Muhammad Naveed Zahid <mnaveedzahid@gmail.com>
2015-11-13 15:14:47 +0500

    Changes for Adding Blank product to estimate and order

--- MPC.Web/Areas/Stores/Scripts/Stores.viewModel.js
+++ MPC.Web/Areas/Stores/Scripts/Stores.viewModel.js
@@ -1346,7 +1346,7 @@ define("stores/stores.viewModel",
                         // check to remove dupplicate color name
                         var count = 0;
                         _.each(selectedStore().companyCMYKColors(), function (color) {
-                            if (color.colorName() == selectedCompanyCMYKColor().colorName() && color.colorId() != selectedCompanyCMYKColor().colorId()) {
+                            if (color.spotColor() == selectedCompanyCMYKColor().spotColor() && color.colorId() != selectedCompanyCMYKColor().colorId()) {
                                 
                                 toastr.error("Color Name already exist.");
                                 flag = false;
