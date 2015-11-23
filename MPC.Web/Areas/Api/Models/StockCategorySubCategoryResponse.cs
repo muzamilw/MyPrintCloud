60be92fe1f56a747f09af2bd52e28273e25f06b0
zohaib-sikander <zohaibsikander@gmail.com>
2015-11-23 18:49:34 +0500

    ccc

---
+++ MPC.Web/Areas/Api/Models/StockCategorySubCategoryResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MPC.MIS.Areas.Api.Models
+{
+    public class StockCategorySubCategoryResponse
+    {
+
+        /// <summary>
+        /// Row Count
+        /// </summary>
+        public int RowCount { get; set; }
+
+        /// <summary>
+        /// List of Stock Categories
+        /// </summary>
+        public IEnumerable<StockCategoryDropDown> StockCategories { get; set; }
+
+
+        /// <summary>
+        /// List of Stock sub Categories
+        /// </summary>
+        public IEnumerable<StockSubCategoryDropDown> StockSubCategories { get; set; }
+    }
+
+
+}
