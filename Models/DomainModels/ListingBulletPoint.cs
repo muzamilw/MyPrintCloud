dd0d15e42a57c4d2fb46d2ee59658b18ff41f1e4
zohaib-sikander <zohaibsikander@gmail.com>
2015-11-26 18:03:10 +0500

    cc

---
+++ Models/DomainModels/ListingBulletPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC.Models.DomainModels
+{
+    public class ListingBulletPoint
+    {
+        public long BulletPointId { get; set; }
+        public string BulletPoint { get; set; }
+        public Nullable<long> ListingId { get; set; }
+
+        public virtual Listing Listing { get; set; }
+    }
+}
