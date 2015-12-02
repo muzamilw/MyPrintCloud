e21bbf0a6f1dc5825161b4623500bf453a484997
zohaib-sikander <zohaibsikander@gmail.com>
2015-12-02 11:27:05 +0500

    ccc

---
+++ MPC.Web/Areas/Api/Models/ReportParamRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MPC.MIS.Areas.Api.Models
+{
+    public class ReportParamRequestModel
+    {
+        public long ReportId { get; set; }
+
+        public int ComboValue { get; set; }
+
+
+         public string ParamDateFromValue { get; set; }
+
+          public string ParamDateToValue { get; set; }
+
+        public string ParamTextBoxValue { get; set; }
+
+    }
+}
