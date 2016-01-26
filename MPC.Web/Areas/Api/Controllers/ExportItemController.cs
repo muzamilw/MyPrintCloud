34503febcd20d42acd98e7896840e82c1f7cbd30
zohaib-sikander <zohaibsikander@gmail.com>
2016-01-26 10:29:17 +0500

    edmx changes

---
+++ MPC.Web/Areas/Api/Controllers/ExportItemController.cs
@@ -0,0 +1,41 @@
+﻿using MPC.Interfaces.Data;
+using MPC.Interfaces.MISServices;
+using MPC.WebBase.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MPC.MIS.Areas.Api.Controllers
+{
+    public class ExportItemController : ApiController
+    {
+
+
+        
+          #region Private
+        private readonly IItemService _itemService;
+     
+        #endregion
+        #region Constructor
+        public ExportItemController(IItemService itemService)
+        {
+            this._itemService = itemService;
+           
+        }
+        #endregion
+
+
+        [CompressFilterAttribute]
+        public string Get(long CompanyId)
+        {
+            string path = _itemService.ExportItems(CompanyId);
+            return path;
+        }
+
+
+     
+    }
+}
