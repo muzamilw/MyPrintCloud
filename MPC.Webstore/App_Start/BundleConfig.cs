2ef2d552392bace5bccb666599f838b625dd599c
awais ghani <awais@myprintcloud.com>
2016-02-23 12:03:26 +0500

    cxvxcvx

--- MPC.Webstore/App_Start/BundleConfig.cs
+++ MPC.Webstore/App_Start/BundleConfig.cs
@@ -5,13 +5,12 @@ namespace MPC.Webstore
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-1.10.2.js",
-                        "~/Scripts/jquery-ui.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-1.10.2.js",
+            //            "~/Scripts/jquery-ui.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/Slider").Include(
                        "~/Scripts/js-image-slider.js",
@@ -19,11 +18,18 @@ namespace MPC.Webstore
                        ));
             bundles.Add(new ScriptBundle("~/bundles/SliderDetail").Include(
                      "~/Scripts/js-image-slider-detail.js"
-                     ));
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*",
-                        "~/Scripts/input.watermark.js"));
+
+            //bundles.Add(
+            //    new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*",
+            //            "~/Scripts/input.watermark.js"));
+
+            //bundles.Add(
+            //    new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*",
+            //            "~/Scripts/input.watermark.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
                      "~/Scripts/toastr.js",
@@ -51,8 +57,8 @@ namespace MPC.Webstore
             bundles.Add(new ScriptBundle("~/bundles/modalEffects").Include(
                      "~/Scripts/modalEffects.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/fancyBox").Include(
-                "~/LightBox/Js/jquery-1.10.1.min.js", "~/LightBox/Js/jquery.fancybox.pack.js", "~/LightBox/Js/jquery.fancybox.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/fancyBox").Include(
+            //    "~/LightBox/Js/jquery-1.10.1.min.js", "~/LightBox/Js/jquery.fancybox.pack.js", "~/LightBox/Js/jquery.fancybox.js"));
             
             bundles.Add(new ScriptBundle("~/pageSpecific"));
 
@@ -79,13 +85,6 @@ namespace MPC.Webstore
                       ));
          
             
-          
-              //bundles.Add(new ScriptBundle("~/bundles/LightBox").Include("~/LightBox1/Js/jquery.js", "~/LightBox1/Js/jquery.lightbox-0.5.js", "~/LightBox1/Js/jquery.lightbox-0.5.min.js", "~/LightBox1/Js/jquery.lightbox-0.5.pack.js", "~/LightBox1/Js/viewport.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/LightBox").Include("~/LightBox/Js/jquery-1.10.1.min.js", "~/LightBox/Js/jquery.fancybox.js", "~/LightBox/Js/jquery.fancybox-thumbs.js", "~/LightBox1/Js/jquery.fancybox-media.js", "~/LightBox1/Js/jquery.fancybox-buttons.js"));
-            // Set EnableOptimizations to false for debugging. For more information,
-
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
         }
     }
