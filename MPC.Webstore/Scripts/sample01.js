d4e351d7c8430378b518bda0722661ee68f08c50
iqramujahid <iqra@myprintcloud.com>
2016-03-29 17:32:13 +0500

    changes

--- MPC.Webstore/Scripts/sample01.js
+++ MPC.Webstore/Scripts/sample01.js
@@ -1,5 +1,5 @@
 ﻿
 //$(document).ready(function () {
    
-    $('.carousel').carousel({ carouselWidth: 700, carouselHeight: 195, directionNav: true, shadow: true, buttonNav: 'bullets' });
+    $('.carousel').carousel({ carouselWidth: 1000, carouselHeight: 195, directionNav: true, shadow: true, buttonNav: 'bullets' });
 //});
